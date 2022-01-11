d43370a79e80c74d2eaa17d2defbbe675d41ec7f
Daniel Lee <daniel.lee@circleci.com>
2022-01-10 20:14:29 -0800

    loading image and data'

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,24 @@
 import _ from "lodash";
 import myName from "./myName";
+import "./style.css";
+import Icon from "./icon.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
     const element = document.createElement("div");
 
     element.innerHTML = myName("Danny");
+    element.classList.add("hello");
+
+    //add the image to existing div
+    const myIcon = new Image(); // equivalent to document.createElement('img')
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
