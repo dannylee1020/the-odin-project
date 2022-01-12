7c69ef3e5ac40f2c3da9fb5991fa38ef9a4ea769
Daniel Lee <daniel.lee@circleci.com>
2022-01-11 19:09:45 -0800

    cleanup see commit history for past

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,64 +1,25 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
+    entry: {
+        index: "./src/index.js",
+        print: "./src/print.js",
     },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
+    mode: "development",
 
-            {
-                test: "/.png|svg|jpg|jpeg|gif)$/i",
-                type: "asset/resource",
-            },
+    devtool: "inline-source-map",
 
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Development",
+        }),
+    ],
 
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ["csv-loader"],
-            },
-
-            {
-                test: /\.xml$/i,
-                use: ["xml-loader"],
-            },
-
-            {
-                test: /\.toml$/i,
-                type: "json",
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: /\.yaml$/i,
-                type: "json",
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test: /\.json5$/i,
-                type: "json",
-                parser: {
-                    parse: json5.parse,
-                },
-            },
-        ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 };
