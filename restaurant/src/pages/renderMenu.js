843e88d418b737f7077f8858679a18ffd9625b22
Daniel Lee <daniel.lee@circleci.com>
2022-01-25 21:30:18 -0800

    script for rendering menu page

--- restaurant/src/pages/renderMenu.js
+++ restaurant/src/pages/renderMenu.js
@@ -1,3 +1,61 @@
+const createList = function (title, price, menu1, menu2, menu3, menu4, menu5) {
+    const course = document.createElement("div");
+    const courseTitle = document.createElement("div");
+    courseTitle.textContent = title;
+    courseTitle.style.fontSize = "25px";
+    courseTitle.style.fontWeight = "bold";
+
+    var menu = document.createElement("div");
+    var item1 = document.createElement("div");
+    var item2 = document.createElement("div");
+    var item3 = document.createElement("div");
+    var item4 = document.createElement("div");
+    var item5 = document.createElement("div");
+
+    var coursePrice = document.createElement("div");
+    coursePrice.setAttribute("id", "coursePrice");
+    coursePrice.textContent = price;
+    coursePrice.style.fontSize = "14px";
+
+    var hr = document.createElement("hr");
+    hr.style.color = "#CCCCCC";
+    hr.style.width = "130%";
+    hr.style.marginTop = "30px";
+    hr.style.marginLeft = "-20px";
+
+    // style ul
+    menu.style.display = "flex";
+    menu.style.flexDirection = "column";
+    menu.style.gap = "12px";
+    menu.style.margin = "30px 0px 30px 0px";
+
+    item1.textContent = menu1;
+    item2.textContent = menu2;
+    item3.textContent = menu3;
+    item4.textContent = menu4;
+    item5.textContent = menu5;
+
+    // append child to parent
+    menu.append(item1, item2, item3, item4, item5);
+    course.append(courseTitle, menu, coursePrice, hr);
+
+    // style course div
+    course.style.display = "flex";
+    course.style.flexDirection = "column";
+    course.style.justifyContent = "center";
+    course.style.textAlign = "center";
+
+    return course;
+};
+
+const createBeverage = function () {
+    const bevContainer = document.createElement("div");
+    bevContainer.setAttribute("id", "bevContainer");
+    // bevContainer.style.backgroundColor = "#FFFBF6";
+
+    return bevContainer;
+};
+
 const aboutMenu = function () {
     const tastingMenu = document.createElement("div");
     tastingMenu.setAttribute("class", "menu");
@@ -28,36 +86,46 @@ const aboutMenu = function () {
     menuTitle.style.fontSize = "35px";
     menuTitle.style.color = "#CD4A3A";
 
-    // course list
+    // create course list
     const courses = document.createElement("div");
 
-    const classic = document.createElement("div");
-    const classicTitle = document.createElement("div");
-    classicTitle.textContent = "The Classic";
-    // // make list
-    var ul = document.createElement("ul");
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
-    var li5 = document.createElement("li");
+    const classic = createList(
+        "The Classic",
+        "32 | Person",
+        "Yellowtail Crudo",
+        "PomoDorini e Basilico",
+        " Vongole",
+        "Pomodoro",
+        "Tiramisu al cucchiaio"
+    );
 
-    var price = document.createElement("div");
-    price.setAttribute("id", "coursePrice");
-    price.textContent = "32|Per Person";
+    const cheese = createList(
+        "Cheese & Truffle",
+        "37 | Person",
+        "Yellowtail Crudo",
+        "Tagliatelle in truffle sauce",
+        "Pomodoro",
+        "Cacio e Pepe",
+        "Tiramisu al cucchiaio"
+    );
 
-    li1.textContent = "Yellowtail Crudo";
-    li2.textContent = "Pomodorini e Basilico";
-    li3.textContent = "Vongole";
-    li4.textContent = "Pomodoro";
-    li5.textContent = "Tiramisu al cucchiaio";
+    const regional = createList(
+        "The Regional",
+        "32 | Person",
+        "Battuta di carne",
+        "Ragú",
+        "Tortellini",
+        "Amatriciana",
+        "Tramisu al cucchiaio"
+    );
 
-    ul.append(li1, li2, li3, li4, li5);
-    classic.append(classicTitle, ul, price);
-    courses.appendChild(classic);
-
+    courses.append(classic, cheese, regional);
+    courses.style.display = "flex";
+    courses.style.justifyContent = "space-around";
+    courses.style.marginTop = "60px";
     // add child to parent
     const container = document.querySelector("#container");
+    // container.style.backgroundColor = "#FFFFFF";
     tastingMenu.appendChild(title);
     tastingMenu.appendChild(subtitle);
     tastingMenu.appendChild(hr);
@@ -65,6 +133,9 @@ const aboutMenu = function () {
 
     container.appendChild(tastingMenu);
     container.appendChild(courses);
+
+    const bevContainer = createBeverage();
+    document.body.appendChild(bevContainer);
 };
 
 export { aboutMenu };
