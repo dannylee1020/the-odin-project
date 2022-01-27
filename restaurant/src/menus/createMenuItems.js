b4d5cbf2e5df3037c7d5b19742ed10f1b66e1b7f
Daniel Lee <daniel.lee@circleci.com>
2022-01-26 21:16:00 -0800

    adding helper functions to create bev list

--- restaurant/src/menus/createMenuItems.js
+++ restaurant/src/menus/createMenuItems.js
@@ -1,4 +1,12 @@
-const createList = function (title, price, menu1, menu2, menu3, menu4, menu5) {
+const createCourse = function (
+    title,
+    price,
+    menu1,
+    menu2,
+    menu3,
+    menu4,
+    menu5
+) {
     const course = document.createElement("div");
     const courseTitle = document.createElement("div");
     courseTitle.textContent = title;
@@ -48,12 +56,47 @@ const createList = function (title, price, menu1, menu2, menu3, menu4, menu5) {
     return course;
 };
 
-const createBeverage = function () {
+const createBeverageList = function (title, obj) {
+    const listContainer = document.createElement("div");
+    const listTitle = document.createElement("div");
+    listTitle.textContent = title;
+
+    listContainer.appendChild(listTitle);
+
+    for (let p in obj) {
+        let itemContainer = document.createElement("div");
+        let itemPrice = document.createElement("div");
+        let item = document.createElement("div");
+        let price = document.createElement("div");
+        let hr = document.createElement("div");
+        let profile = document.createElement("div");
+
+        item.textContent = obj[p][0];
+        price.textContent = obj[p][1];
+        profile.textContent = obj[p][2];
+
+        itemPrice.append(item, price);
+        itemContainer.append(itemPrice, hr, profile);
+        listContainer.appendChild(itemContainer);
+    }
+
+    return listContainer;
+};
+
+const createBeverageSection = function () {
     const bevContainer = document.createElement("div");
     bevContainer.setAttribute("id", "bevContainer");
-    // bevContainer.style.backgroundColor = "#FFFBF6";
+    // bevContainer.style.backgroundColor = "#E2DAC7";
+
+    const beerObj = {
+        first: ["Peroni", "$6", "Lager (325ml)"],
+        second: ["Angelo Poretti", "$7", "Premium Lager (325ml.)"],
+    };
+
+    const beerList = createBeverageList("Beer", beerObj);
+    bevContainer.appendChild(beerList);
 
     return bevContainer;
 };
 
-export { createList, createBeverage };
+export { createCourse, createBeverageSection };
