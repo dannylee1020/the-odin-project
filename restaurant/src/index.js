7bbc0e71dace5721c727bbd4822cd65428f2c2a8
Daniel Lee <daniel.lee@circleci.com>
2022-01-28 19:29:26 -0800

    testing contact page

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -1,28 +1,31 @@
 import { aboutPage } from "./pages/renderAbout.js";
 import { aboutMenu } from "./pages/renderMenu.js";
+import { aboutContact } from "./pages/renderContact";
 
-const aboutTab = document.querySelector("#about");
-const menuTab = document.querySelector("#menu");
-const contactTab = document.querySelector("#contact");
+// const aboutTab = document.querySelector("#about");
+// const menuTab = document.querySelector("#menu");
+// const contactTab = document.querySelector("#contact");
 
-// const renderPage = function (func) {
+// // const renderPage = function (func) {
+// //     let container = document.querySelector("#container");
+// //     let page = func();
+// //     container.replaceChildren(page);
+// // };
+
+// let homePage = aboutPage();
+// let container = document.querySelector("#container");
+// container.append(homePage);
+
+// aboutTab.addEventListener("click", function () {
 //     let container = document.querySelector("#container");
-//     let page = func();
-//     container.replaceChildren(page);
-// };
+//     let about = aboutPage();
+//     container.replaceChildren(about);
+// });
 
-let homePage = aboutPage();
-let container = document.querySelector("#container");
-container.append(homePage);
+// menuTab.addEventListener("click", function () {
+//     let container = document.querySelector("#container");
+//     let menu = aboutMenu();
+//     container.replaceChildren(menu);
+// });
 
-aboutTab.addEventListener("click", function () {
-    let container = document.querySelector("#container");
-    let about = aboutPage();
-    container.replaceChildren(about);
-});
-
-menuTab.addEventListener("click", function () {
-    let container = document.querySelector("#container");
-    let menu = aboutMenu();
-    container.replaceChildren(menu);
-});
+aboutContact();
