b723db8fd757211199592f373918c800482a341d
Daniel Lee <daniel.lee@circleci.com>
2022-01-30 15:52:14 -0800

    adding background image

--- restaurant/src/pages/renderContact.js
+++ restaurant/src/pages/renderContact.js
@@ -1,13 +1,16 @@
-import header_img from "../img/uovo_inside.jpeg";
+import "../style/styleTabs.css";
+import "../style/styleContact.css";
 
 const aboutContact = function () {
     const contactContainer = document.createElement("div");
+    contactContainer.setAttribute("id", "contactContainer");
     const contactHeader = document.createElement("div");
+    contactHeader.setAttribute("id", "contactHeader");
     const contactBody = document.createElement("div");
     const contactForm = document.createElement("div");
 
-    const headerImage = new Image(50, 50);
-    headerImage.src = header_img;
+    // const headerImage = new Image(50, 50);
+    // headerImage.src = header_img;
 
     // working on header
     let headerTitle = document.createElement("div");
@@ -22,7 +25,7 @@ const aboutContact = function () {
     hr.style.width = "20%";
     headerSub.textContent = "Do not hesitate to get in touch at any time.";
 
-    contactHeader.append(headerImage, headerTitle, hr, headerSub);
+    contactHeader.append(headerTitle, hr, headerSub);
     contactContainer.append(contactHeader);
     document.querySelector("#container").append(contactContainer);
 };
