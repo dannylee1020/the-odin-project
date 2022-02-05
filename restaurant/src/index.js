36baf1c0ecae7b60d708e0e8bfe5a67e76540138
Daniel Lee <daniel.lee@circleci.com>
2022-02-05 15:13:26 -0800

    logic for switcing tab

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -6,26 +6,30 @@ const aboutTab = document.querySelector("#about");
 const menuTab = document.querySelector("#menu");
 const contactTab = document.querySelector("#contact");
 
-// // const renderPage = function (func) {
-// //     let container = document.querySelector("#container");
-// //     let page = func();
-// //     container.replaceChildren(page);
-// // };
-
-// let homePage = aboutPage();
-// let container = document.querySelector("#container");
-// container.append(homePage);
-
-// aboutTab.addEventListener("click", function () {
+// const renderPage = function (func) {
 //     let container = document.querySelector("#container");
-//     let about = aboutPage();
-//     container.replaceChildren(about);
-// });
+//     let page = func();
+//     container.replaceChildren(page);
+// };
 
-// menuTab.addEventListener("click", function () {
-//     let container = document.querySelector("#container");
-//     let menu = aboutMenu();
-//     container.replaceChildren(menu);
-// });
+let homePage = aboutPage();
+let container = document.querySelector("#container");
+container.append(homePage);
 
-aboutContact();
+aboutTab.addEventListener("click", function () {
+    let container = document.querySelector("#container");
+    let about = aboutPage();
+    container.replaceChildren(about);
+});
+
+menuTab.addEventListener("click", function () {
+    let container = document.querySelector("#container");
+    let menu = aboutMenu();
+    container.replaceChildren(menu);
+});
+
+contactTab.addEventListener("click", function () {
+    let container = document.querySelector("#container");
+    let contact = aboutContact();
+    container.replaceChildren(contact);
+});
