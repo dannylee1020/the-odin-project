9aba27745ae270b0c8f7cc72304642143d72ec2e
Daniel Lee <daniel.lee@circleci.com>
2022-02-05 14:15:47 -0800

    scripts for making requests to google maps api

--- restaurant/src/components/createMap.js
+++ restaurant/src/components/createMap.js
@@ -1,14 +1,36 @@
 const createMap = function initMap() {
-  const uovo = {lat: 34.02422007490653, lng: -118.49753104089724 };
-  const map = new google.maps.Map(document.querySelector('.map'), {
-    zoom: 4,
-    center: uovo,
-  });
+    // window.initMap = initMap;
+    const uovo = { lat: 34.016050923377136, lng: -118.49753104089724 };
+    const map = new google.maps.Map(document.querySelector("#map"), {
+        zoom: 15,
+        center: uovo,
+    });
 
-  const marker = new.google.maps.Marker({
-    position: uovo,
-    map: map
-  })
-}
+    const marker = new google.maps.Marker({
+        position: uovo,
+        map: map,
+    });
+};
 
-export {createMap};
+// const addScript = function (src, callback) {
+//     var s = document.createElement("script");
+//     s.setAttribute("src", src);
+//     s.onload = callback;
+//     return s;
+// };
+
+const buildMap = function () {
+    var script = document.createElement("script");
+    script.text = createMap;
+    document.head.appendChild(script);
+
+    let apiKey = "AIzaSyDHbrkqTmxNmG0a1o5ht5TId-u37Ph4tRA";
+    let src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    let s = document.createElement("script");
+    s.setAttribute("src", src);
+    s.async = true;
+
+    return s;
+};
+
+export { buildMap };
