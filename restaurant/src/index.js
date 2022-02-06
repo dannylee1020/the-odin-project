917099741192ecab9baf67d202762b9b12d6eabd
Daniel Lee <daniel.lee@circleci.com>
2022-02-06 15:54:02 -0800

    move tab logic to a module"

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -1,35 +1,3 @@
-import { aboutPage } from "./pages/renderAbout.js";
-import { aboutMenu } from "./pages/renderMenu.js";
-import { aboutContact } from "./pages/renderContact";
+import { updateContent } from "./controllers/updateContent";
 
-const aboutTab = document.querySelector("#about");
-const menuTab = document.querySelector("#menu");
-const contactTab = document.querySelector("#contact");
-
-// const renderPage = function (func) {
-//     let container = document.querySelector("#container");
-//     let page = func();
-//     container.replaceChildren(page);
-// };
-
-let homePage = aboutPage();
-let container = document.querySelector("#container");
-container.append(homePage);
-
-aboutTab.addEventListener("click", function () {
-    let container = document.querySelector("#container");
-    let about = aboutPage();
-    container.replaceChildren(about);
-});
-
-menuTab.addEventListener("click", function () {
-    let container = document.querySelector("#container");
-    let menu = aboutMenu();
-    container.replaceChildren(menu);
-});
-
-contactTab.addEventListener("click", function () {
-    let container = document.querySelector("#container");
-    let contact = aboutContact();
-    container.replaceChildren(contact);
-});
+updateContent();
