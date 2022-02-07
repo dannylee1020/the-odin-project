63b2b5296907b2ce62a72fcbfbecf87c55940007
Daniel Lee <daniel.lee@circleci.com>
2022-02-06 22:27:06 -0800

    adding footer'

--- restaurant/src/pages/renderAbout.js
+++ restaurant/src/pages/renderAbout.js
@@ -1,14 +1,17 @@
 import "../style/styleTabs.css";
 import "../style/styleAbout.css";
 import { buildContent, buildBody } from "../components/buildAbout";
+import { buildFooter } from "../components/builders/createFooter";
 
 const aboutPage = function () {
     const pageContainer = document.createElement("div");
+    pageContainer.setAttribute("id", "page-container");
     const content = buildContent();
     const introDiv = buildBody();
+    const footer = buildFooter();
 
     // adding all elements to pageContainer
-    pageContainer.append(content, introDiv);
+    pageContainer.append(content, introDiv, footer);
 
     return pageContainer;
 };
--- restaurant/src/pages/renderMenu.js
+++ restaurant/src/pages/renderMenu.js
@@ -1,12 +1,14 @@
 import { tastingMenu } from "../components/buildMenu";
 import { createBeverageSection } from "../components/builders/createMenuItems";
+import { buildFooter } from "../components/builders/createFooter";
 
 const aboutMenu = function () {
     const menuPageContainer = document.createElement("div");
     const tastingContainer = tastingMenu();
     const bevContainer = createBeverageSection();
+    const footer = buildFooter();
 
-    menuPageContainer.append(tastingContainer, bevContainer);
+    menuPageContainer.append(tastingContainer, bevContainer, footer);
     return menuPageContainer;
 };
 
