9e26b95acaa9aee5448c2f001a6994e3688c77cf
Daniel Lee <daniel.lee@circleci.com>
2022-02-06 16:29:53 -0800

    using modules to render pages

--- restaurant/src/pages/renderAbout.js
+++ restaurant/src/pages/renderAbout.js
@@ -1,93 +1,11 @@
 import "../style/styleTabs.css";
 import "../style/styleAbout.css";
-import first_src from "../img/fresh_pasta_1.jpeg";
-import second_src from "../img/fresh_pasta_2.jpeg";
-import third_src from "../img/fresh_pasta_3.jpeg";
+import { buildContent, buildBody } from "../components/buildAbout";
 
 const aboutPage = function () {
     const pageContainer = document.createElement("div");
-    const content = document.createElement("div");
-    content.setAttribute("id", "content");
-
-    const name = document.createElement("div");
-    name.setAttribute("id", "name");
-    name.style.fontSize = "60px";
-    name.style.margin = "100px";
-
-    const description = document.createElement("div");
-    description.setAttribute("id", "description");
-    description.style.width = "550px";
-    description.style.fontSize = "40px";
-    description.style.marginBottom = "20px";
-    description.style.textAlign = "center";
-
-    const origin = document.createElement("div");
-    origin.setAttribute("id", "origin");
-    origin.style.fontSize = "25px";
-
-    // set textContent
-    name.textContent = "UOVO";
-    description.textContent =
-        "the one place outside of Italy that serves fresh pasta made in Italy";
-    origin.textContent = "- by hand daily, by our team in Bologna.";
-
-    // generate intro
-    const introDiv = document.createElement("div");
-    introDiv.style.backgroundColor = "#F5F3F1";
-    introDiv.style.textAlign = "center";
-    introDiv.style.height = "100%";
-    introDiv.style.border = "None";
-    introDiv.style.position = "absolute";
-
-    const title = document.createElement("h2");
-    title.style.color = "#CB7152";
-    title.style.paddingTop = "20px";
-    title.style.fontSize = "50px";
-    title.textContent = "Our pasta";
-
-    const intro = document.createElement("p");
-    intro.style.wordBreak = "break-word";
-    intro.style.color = "black";
-    intro.style.fontWeight = "10px";
-    intro.style.fontSize = "45px";
-    intro.style.margin = "15px 150px 50px 150px";
-    intro.textContent = `is handmade in our kitchen in Bologna, Italy by "sfogline" who use traditional methods of sheeting
-         and cutting -- none of our pasta is extruded. It's prepared fresh daily, using a type of egg
-         that's available only in Italy and is specially produced for pasta. The reuslt is a superior noodle
-         that is the foundation for our classic Italian dishes.`;
-
-    // add images at the bottom
-    const imgContainer = document.createElement("div");
-    const foodImage1 = new Image(150, 150);
-    const foodImage2 = new Image(150, 150);
-    const foodImage3 = new Image(150, 150);
-
-    // add image source
-    foodImage1.src = first_src;
-    foodImage2.src = second_src;
-    foodImage3.src = third_src;
-
-    // add image style
-    foodImage1.style.borderRadius = "50%";
-    foodImage2.style.borderRadius = "50%";
-    foodImage3.style.borderRadius = "50%";
-
-    foodImage2.style.margin = "30px 50px 0px 50px";
-
-    // adding child to parent
-    imgContainer.appendChild(foodImage1);
-    imgContainer.appendChild(foodImage2);
-    imgContainer.appendChild(foodImage3);
-
-    // // append all child to parent
-    introDiv.appendChild(title);
-    introDiv.appendChild(intro);
-    introDiv.appendChild(imgContainer);
-
-    // add elements to parent
-    content.appendChild(name);
-    content.appendChild(description);
-    content.appendChild(origin);
+    const content = buildContent();
+    const introDiv = buildBody();
 
     // adding all elements to pageContainer
     pageContainer.append(content, introDiv);
--- restaurant/src/pages/renderMenu.js
+++ restaurant/src/pages/renderMenu.js
@@ -1,92 +1,12 @@
-import {
-    createBeverageSection,
-    createCourse,
-} from "../components/createMenuItems";
+import { tastingMenu } from "../components/buildMenu";
+import { createBeverageSection } from "../components/builders/createMenuItems";
 
 const aboutMenu = function () {
     const menuPageContainer = document.createElement("div");
-    const menuContainer = document.createElement("div");
-    const tastingMenu = document.createElement("div");
-    tastingMenu.setAttribute("class", "menu");
-
-    const title = document.createElement("div");
-    title.setAttribute("class", "title");
-    title.textContent = "Every noodle handmade by us in Italy";
-    title.style.textAlign = "center";
-    title.style.fontSize = "40px";
-    title.style.fontWeight = "500";
-    title.style.margin = "25px 0px 0px 0px";
-
-    const subtitle = document.createElement("div");
-    subtitle.textContent = "Shippped overnight | Nothing extruded";
-    subtitle.style.textAlign = "center";
-    subtitle.style.fontSize = "25px";
-    subtitle.style.margin = "20px 0px 10px 0px";
-
-    const hr = document.createElement("hr");
-    hr.style.color = "#CCCCCC";
-    hr.style.margin = "60px 0px 30px 0px";
-
-    // tasting menu section
-    const menuTitle = document.createElement("div");
-    menuTitle.setAttribute("class", "menuTitle");
-    menuTitle.textContent = "- TASTING MENUS -";
-    menuTitle.style.textAlign = "center";
-    menuTitle.style.fontSize = "35px";
-    menuTitle.style.color = "#CD4A3A";
-
-    // create course list
-    const courses = document.createElement("div");
-
-    const classic = createCourse(
-        "The Classic",
-        "32 | Person",
-        "Yellowtail Crudo",
-        "PomoDorini e Basilico",
-        " Vongole",
-        "Pomodoro",
-        "Tiramisu al cucchiaio"
-    );
-
-    const cheese = createCourse(
-        "Cheese & Truffle",
-        "37 | Person",
-        "Yellowtail Crudo",
-        "Tagliatelle in truffle sauce",
-        "Pomodoro",
-        "Cacio e Pepe",
-        "Tiramisu al cucchiaio"
-    );
-
-    const regional = createCourse(
-        "The Regional",
-        "32 | Person",
-        "Battuta di carne",
-        "Ragú",
-        "Tortellini",
-        "Amatriciana",
-        "Tramisu al cucchiaio"
-    );
-
-    courses.append(classic, cheese, regional);
-    courses.style.display = "flex";
-    courses.style.justifyContent = "space-around";
-    courses.style.marginTop = "60px";
-
-    // add child to parent
-
-    tastingMenu.appendChild(title);
-    tastingMenu.appendChild(subtitle);
-    tastingMenu.appendChild(hr);
-    tastingMenu.appendChild(menuTitle);
-
-    menuContainer.appendChild(tastingMenu);
-    menuContainer.appendChild(courses);
-    menuContainer.setAttribute("id", "menu");
-
+    const tastingContainer = tastingMenu();
     const bevContainer = createBeverageSection();
-    menuPageContainer.append(menuContainer, bevContainer);
 
+    menuPageContainer.append(tastingContainer, bevContainer);
     return menuPageContainer;
 };
 
