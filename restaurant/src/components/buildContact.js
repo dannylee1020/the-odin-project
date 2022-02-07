24a3ccff87c1664259c9439a9cafec0210b28a47
Daniel Lee <daniel.lee@circleci.com>
2022-02-06 16:29:27 -0800

    modularizing page building scripts

---
+++ restaurant/src/components/buildContact.js
@@ -0,0 +1,137 @@
+import { buildMap } from "./builders/createMap";
+
+const header = function () {
+    const contactHeader = document.createElement("div");
+    contactHeader.setAttribute("id", "contactHeader");
+    // working on header
+    let headerTitle = document.createElement("div");
+    let hr = document.createElement("hr");
+    let headerSub = document.createElement("div");
+
+    headerTitle.textContent = "Contact";
+    headerTitle.style.color = "white";
+    headerTitle.style.fontSize = "30px";
+    headerTitle.style.fontWeight = "bold";
+    headerTitle.style.marginTop = "35px";
+
+    hr.style.border = "2px solid #CB7152";
+    hr.style.width = "15%";
+    hr.style.margin = "20px 0 20px 0";
+
+    headerSub.textContent = "Do not hesitate to get in touch at any time.";
+    headerSub.style.color = "white";
+    headerSub.style.margin = "10px 0 15px 0 ";
+    headerSub.style.fontSize = "20px";
+    contactHeader.append(headerTitle, hr, headerSub);
+
+    return contactHeader;
+};
+
+const body = function () {
+    const contactBody = document.createElement("div");
+    contactBody.setAttribute("id", "contactBody");
+    // working on body
+    contactBody.style.backgroundColor = "#F5F3F1";
+    contactBody.style.height = "600px";
+
+    // hour section
+    let hourContainer = document.createElement("div");
+    let days = document.createElement("div");
+    let hour = document.createElement("div");
+    let clock = document.createElement("i");
+    clock.setAttribute("class", "far fa-clock");
+
+    clock.style.color = "#AD5845";
+    clock.style.fontSize = "60px";
+    clock.style.marginBottom = "20px";
+
+    days.textContent = "Mon - Sat";
+    days.style.fontSize = "20px";
+    hour.textContent = "11:30am - 10:00pm";
+    hour.style.fontSize = "20px";
+    hour.style.margin = "10px 0 10px 0";
+    hourContainer.style.display = "flex";
+    hourContainer.style.flexDirection = "column";
+    hourContainer.style.alignItems = "center";
+    hourContainer.style.marginTop = "40px";
+    hourContainer.append(clock, days, hour);
+
+    // rule section
+    let ruleContainer = document.createElement("div");
+    let warning = document.createElement("div");
+    warning.setAttribute("class", "fas fa-exclamation-triangle");
+
+    let rules = document.createElement("div");
+    let holidays = document.createElement("div");
+
+    warning.style.color = "#AD5845";
+    warning.style.fontSize = "60px";
+    warning.style.marginBottom = "20px";
+
+    rules.textContent = `We DO NOT take reservations. Guests will be seated on a first-come,
+                           first-served basis.`;
+    rules.style.margin = "10px 0 10px 0";
+    rules.style.fontSize = "20px";
+    holidays.textContent = `We will be CLOSED on Labor Day, Thanksgiving,
+                              Christmas and New Years Day`;
+    holidays.style.fontSize = "20px";
+
+    ruleContainer.style.width = "230px";
+    ruleContainer.style.wordBreak = "break-word";
+    ruleContainer.style.textAlign = "center";
+    ruleContainer.style.display = "flex";
+    ruleContainer.style.flexDirection = "column";
+    ruleContainer.style.alignItems = "center";
+    ruleContainer.style.marginTop = "40px";
+    ruleContainer.append(warning, rules, holidays);
+
+    // media inquiries
+    let mediaContainer = document.createElement("div");
+    let inquiries = document.createElement("div");
+    let media = document.createElement("i");
+    media.setAttribute("class", "far fa-newspaper");
+    let email = document.createElement("div");
+    let number = document.createElement("div");
+
+    media.style.color = "#AD5845";
+    media.style.fontSize = "60px";
+    media.style.marginBottom = "20px";
+    inquiries.textContent = "Inquiries";
+    inquiries.style.marginBottom = "10px";
+    inquiries.style.fontSize = "30px";
+    email.textContent = "uovola@example.com";
+    email.style.fontSize = "20px";
+    email.style.marginBottom = "5px";
+    number.textContent = "call us! : 213-999-9999";
+    number.style.fontSize = "20px";
+
+    mediaContainer.style.display = "flex";
+    mediaContainer.style.flexDirection = "column";
+    mediaContainer.style.alignItems = "center";
+    mediaContainer.style.marginTop = "30px";
+    mediaContainer.append(media, inquiries, email, number);
+
+    contactBody.style.display = "flex";
+    contactBody.style.justifyContent = "space-around";
+    contactBody.append(hourContainer, ruleContainer, mediaContainer);
+
+    return contactBody;
+};
+
+const location = function () {
+    const mapContainer = document.createElement("div");
+    const mapFrame = document.createElement("div");
+    mapFrame.setAttribute("id", "map");
+    // add google map
+    let s = buildMap();
+    mapFrame.style.height = "500px";
+    mapFrame.style.width = "100%";
+    // mapFrame.style.marginBottom = "40px";
+    mapContainer.style.display = "flex";
+    mapContainer.style.justifyContent = "center";
+    mapContainer.append(mapFrame, s);
+
+    return mapContainer;
+};
+
+export { header, body, location };
---
+++ restaurant/src/components/buildMenu.js
@@ -0,0 +1,82 @@
+import { createCourse } from "./builders/createMenuItems";
+
+const tastingMenu = function () {
+    const menuContainer = document.createElement("div");
+    const tastingMenu = document.createElement("div");
+    tastingMenu.setAttribute("class", "menu");
+
+    const title = document.createElement("div");
+    title.setAttribute("class", "title");
+    title.textContent = "Every noodle handmade by us in Italy";
+    title.style.textAlign = "center";
+    title.style.fontSize = "40px";
+    title.style.fontWeight = "500";
+    title.style.margin = "25px 0px 0px 0px";
+
+    const subtitle = document.createElement("div");
+    subtitle.textContent = "Shippped overnight | Nothing extruded";
+    subtitle.style.textAlign = "center";
+    subtitle.style.fontSize = "25px";
+    subtitle.style.margin = "20px 0px 10px 0px";
+
+    const hr = document.createElement("hr");
+    hr.style.color = "#CCCCCC";
+    hr.style.margin = "60px 0px 30px 0px";
+
+    // tasting menu section
+    const menuTitle = document.createElement("div");
+    menuTitle.setAttribute("class", "menuTitle");
+    menuTitle.textContent = "- TASTING MENUS -";
+    menuTitle.style.textAlign = "center";
+    menuTitle.style.fontSize = "35px";
+    menuTitle.style.color = "#CD4A3A";
+
+    tastingMenu.appendChild(title);
+    tastingMenu.appendChild(subtitle);
+    tastingMenu.appendChild(hr);
+    tastingMenu.appendChild(menuTitle);
+    const courses = document.createElement("div");
+
+    const classic = createCourse(
+        "The Classic",
+        "32 | Person",
+        "Yellowtail Crudo",
+        "PomoDorini e Basilico",
+        " Vongole",
+        "Pomodoro",
+        "Tiramisu al cucchiaio"
+    );
+
+    const cheese = createCourse(
+        "Cheese & Truffle",
+        "37 | Person",
+        "Yellowtail Crudo",
+        "Tagliatelle in truffle sauce",
+        "Pomodoro",
+        "Cacio e Pepe",
+        "Tiramisu al cucchiaio"
+    );
+
+    const regional = createCourse(
+        "The Regional",
+        "32 | Person",
+        "Battuta di carne",
+        "Ragú",
+        "Tortellini",
+        "Amatriciana",
+        "Tramisu al cucchiaio"
+    );
+
+    courses.append(classic, cheese, regional);
+    courses.style.display = "flex";
+    courses.style.justifyContent = "space-around";
+    courses.style.marginTop = "60px";
+
+    menuContainer.appendChild(tastingMenu);
+    menuContainer.appendChild(courses);
+    menuContainer.setAttribute("id", "menu");
+
+    return menuContainer;
+};
+
+export { tastingMenu };
