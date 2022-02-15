630c098208ae041d1d7399f6763785e4e4c3cd9a
Daniel Lee <daniel.lee@circleci.com>
2022-02-14 22:09:16 -0800

    declaring font

--- todo/src/components/buildHeader.js
+++ todo/src/components/buildHeader.js
@@ -9,16 +9,19 @@ const buildHeader = function () {
 
     header.style.display = "flex";
     header.style.width = "100%";
-    header.style.justifyContent = "center";
+    header.style.justifyContent = "left";
     header.style.alignItems = "center";
     header.style.backgroundColor = "#DB4C3F";
     header.style.height = "100px";
 
-    icon.setAttribute("class", "fa-solid fa-check-double");
+    icon.setAttribute("class", "fas fa-check-double");
     icon.style.fontSize = "40px";
+    icon.style.color = "white";
+    icon.style.marginLeft = "50px";
 
     name.textContent = "ToDo";
-    name.style.marginLeft = "100px";
+    name.style.fontFamily = "Ubuntu";
+    name.style.marginLeft = "20px";
     name.style.color = "white";
     name.style.fontSize = "30px";
 
