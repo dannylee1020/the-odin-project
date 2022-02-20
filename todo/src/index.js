9e0cc95731a63aa2fe342c2a09706c19964241ef
Daniel Lee <daniel.lee@circleci.com>
2022-02-20 15:39:34 -0800

    entry point

--- todo/src/index.js
+++
@@ -1,7 +0,0 @@
-import { buildHeader } from "./components/buildHeader";
-import { buildSideBar } from "./components/buildSideBar";
-
-const header = buildHeader();
-const sideBar = buildSideBar();
-
-document.body.append(header, sideBar);
