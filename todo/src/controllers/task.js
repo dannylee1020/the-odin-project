817db8ba177c408654ab748b051b3d9f0dc261f8
Daniel Lee <daniel.lee@circleci.com>
2022-02-24 23:02:37 -0800

    todo for tasks"

---
+++ todo/src/controllers/task.js
@@ -0,0 +1,9 @@
+const Task = () => {
+    const addForm = function () {
+        //* when Add task is clicked, replace it with the form
+    };
+
+    const addTask = function () {
+        //* when add in the form is clicked, add the task to the list
+    };
+};
