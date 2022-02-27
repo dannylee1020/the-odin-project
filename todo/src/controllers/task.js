50646d37ad5dc1092f0fcdf2368b4e8833db8389
Daniel Lee <daniel.lee@circleci.com>
2022-02-26 23:25:23 -0800

    adding form logic to the task

--- todo/src/controllers/task.js
+++ todo/src/controllers/task.js
@@ -1,9 +1,32 @@
 const Task = () => {
     const addForm = function () {
-        //* when Add task is clicked, replace it with the form
+        const form = `
+          <div id='form-container'>
+            <input id='task-input'>
+            <div id='task-buttons'>
+              <div id='submit-task'>Add</div>
+              <div id='cancel-task'>Cancel</div>
+            </div>
+          </div>
+          `;
+
+        const inboxContent = document.querySelector("#inbox-content");
+        const taskContainer = document.querySelector("#task-container");
+
+        taskContainer.addEventListener("click", (e) => {
+            e.currentTarget.style.display = "None";
+            inboxContent.innerHTML += form;
+        });
     };
+
+    //TODO: style form with DOM strings
+    //
 
     const addTask = function () {
         //* when add in the form is clicked, add the task to the list
     };
+
+    return { addForm };
 };
+
+export { Task };
