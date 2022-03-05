2f0f72583c0c745577ec7494ccbb9046270fb0ea
Daniel Lee <daniel.lee@circleci.com>
2022-03-04 17:41:25 -0800

    add page switching logic with preserving contents on the page

--- todo/src/controllers/pages.js
+++ todo/src/controllers/pages.js
@@ -1,39 +1,82 @@
 const Pages = () => {
-    const removeContent = function (parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
+    // ? instead of switching tab contents, make function for individual page?
+    // ! refactor code to more modules
+
+    const addPage = function (page) {
+        let taskPage = document.querySelector("#task-page");
+
+        if (page === "Inbox") {
+            let inboxHTML = `
+            <div id='inbox-content' data-content='inbox'>
+                <div id='inbox-title'> Inbox </div>
+                <div id='task-container'>
+                    <i class='fas fa-plus'></i>
+                    <div id='add-task'> Add Task</div>
+                </div>
+            <div>
+            `;
+
+            taskPage.insertAdjacentHTML("afterbegin", inboxHTML);
+        } else if (page === "Today") {
+            let todayHTML = `
+            <div id='today-content' data-content='today'>
+                <div id='today-title'>Today </div>
+                <div id='today-task'></div>
+            </div>
+            `;
+
+            taskPage.insertAdjacentHTML("afterbegin", todayHTML);
+        } else {
+            let upcomingHTML = `
+            <div id='upcoming-content' data-content='upcoming'>
+                <div id='upcoming-title'>This Week</div>
+                <div id='upcoming-task'></div>
+            </div>
+            `;
+
+            taskPage.insertAdjacentHTML("afterbegin", upcomingHTML);
         }
     };
 
-    const loadContent = function (pageName) {
-        const taskPage = document.querySelector("#task-page");
-        if (pageName === "Inbox") {
-            removeContent(taskPage);
-            taskPage.innerHTML += `
-              <div id='inbox-content'>
-                  <div id='inbox-title'> Inbox </div>
-                  <div id='task-container'>
-                      <i class='fas fa-plus'></i>
-                      <div id='add-task'> Add Task</div>
-                  </div>
-              <div>
-          `;
-        } else if (pageName === "Today") {
-            removeContent(taskPage);
-            taskPage.innerHTML += `
-              <div id='today-content'>
-                <div id='today-title'>Today </div>
-                <div id='today-task'></div>
-              </div>
-          `;
+    const togglePage = function (page) {
+        const contentPage = document.querySelectorAll("[data-content]");
+
+        contentPage.forEach((item) => {
+            item.style.display = "none";
+        });
+
+        if (page === "Inbox") {
+            document.querySelector("#inbox-content").style.display = "block";
+        } else if (page === "Today") {
+            document.querySelector("#today-content").style.display = "block";
         } else {
-            removeContent(taskPage);
-            taskPage.innerHTML += `
-            <div id='upcoming-content'>
-              <div id='upcoming-title'>This Week</div>
-              <div id='upcoming-task'></div>
-            </div>
-          `;
+            document.querySelector("#upcoming-content").style.display = "block";
+        }
+    };
+
+    const loadContent = function (page) {
+        let taskPage = document.querySelector("#task-page");
+        if (page === "Inbox") {
+            if (taskPage.querySelector("#inbox-content") === null) {
+                addPage("Inbox");
+                togglePage("Inbox");
+            } else {
+                togglePage("Inbox");
+            }
+        } else if (page === "Today") {
+            if (taskPage.querySelector("#today-content") === null) {
+                addPage("Today");
+                togglePage("Today");
+            } else {
+                togglePage("Today");
+            }
+        } else {
+            if (taskPage.querySelector("#upcoming-content") === null) {
+                addPage("Upcoming");
+                togglePage("Upcoming");
+            } else {
+                togglePage("Upcoming");
+            }
         }
     };
 
