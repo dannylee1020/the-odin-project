462e5956be8d5454878ae7391e3db95c7766e1f0
Daniel Lee <daniel.lee@circleci.com>
2022-03-14 21:57:03 -0700

    add toggle function

--- todo/src/controllers/project.js
+++ todo/src/controllers/project.js
@@ -1,13 +1,17 @@
+// TODO adding project to the list not working
+// TODO callback functions executing only once
+// TODO style project textarea
+
 const Project = () => {
     // callback when add project button is clicked
     const addForm = () => {
         let addProject = document.querySelector("#add-project");
         let formHTML = `
-            <div id="form-container">
+            <div id="project-form">
                 <textarea id="project-input"></textarea>
                 <div id="project-bttns">
-                    <button id="add-bttn">Add Project</button>
-                    <button id='cancel-bttn'>Cancel</button>
+                    <button id="project-add">Add Project</button>
+                    <button id='project-cancel'>Cancel</button>
                 </div>
             </div>
         `;
@@ -15,37 +19,56 @@ const Project = () => {
         addProject.insertAdjacentHTML("beforebegin", formHTML);
 
         // toggle add project button
-        addProject.style.display = "none";
+        toggleElement(addProject, "off");
+    };
+
+    const toggleElement = (el, toggle, type) => {
+        if (toggle != "on") {
+            el.style.display = "none";
+        }
+        el.style.display = type;
     };
 
     // callback when add project in the form is clicked
-    const addProject = (pr) => {
-        let addProject = document.querySelector("#add-project");
+    const addProject = () => {
+        let addProjectElement = document.querySelector("#add-project");
+        let projectInput = document.querySelector("#project-input");
 
         // create new project element
         let project = document.createElement("div");
         let projectIcon = document.createElement("i");
         projectIcon.setAttribute("class", "fas fa-list-check");
         let projectName = document.createElement("div");
-        projectName.textContent = pr;
+        projectName.textContent = projectInput.select();
 
         // add new project to the list
         project.append(projectIcon, projectName);
-        project.before(addProject);
+        addProjectElement.before(project);
 
         // style elements
+        // toggleElement(project, "on", "flex");
     };
 
     const submitProject = () => {
-        let submit = document.querySelector("#add-bttn");
-        submit.addEventListener("click", addProject);
+        let submit = document.querySelector("#project-add");
+        let addProjectElement = document.querySelector("#add-project");
+        let projectForm = document.querySelector("#project-form");
+
+        submit.addEventListener("click", function () {
+            addProject();
+            toggleElement(addProjectElement, "on", "flex");
+            toggleElement(projectForm, "off");
+        });
     };
 
     const cancelProject = () => {
-        let cancel = document.querySelector("#cancel-bttn");
+        let cancel = document.querySelector("#project-cancel");
+        let addProjectElement = document.querySelector("#add-project");
+        let projectForm = document.querySelector("#project-form");
+
         cancel.addEventListener("click", function () {
-            document.querySelector("#add-project").style.display = "flex";
-            document.querySelector("#form-container").style.display = "none";
+            toggleElement(addProjectElement, "on", "flex");
+            toggleElement(projectForm, "off");
         });
     };
 
@@ -56,9 +79,6 @@ const Project = () => {
             submitProject();
             cancelProject();
         });
-
-        // submitProject();
-        // cancelProject();
     };
 
     return { initProjectLogic };
