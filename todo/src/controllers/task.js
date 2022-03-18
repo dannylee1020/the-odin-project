6d5ba792c41e0cf1a0292af4ddf66caf5f575522
Daniel Lee <daniel.lee@circleci.com>
2022-03-17 22:56:50 -0700

    working on setting date functionality

--- todo/src/controllers/task.js
+++ todo/src/controllers/task.js
@@ -1,7 +1,7 @@
 import "../style/tasks.css";
 
-// ? create separate task.css to handle styling instead of DOM?
-// ! refactor code to have another module responsible for running all logics
+// TODO add setting date to task functionaliy
+// TODO add x to each task for deletion
 
 const Task = () => {
     const initTaskLogic = function () {
@@ -29,7 +29,6 @@ const Task = () => {
                 // reset text area
                 document.querySelector("#task-input").value = " ";
             }
-            // styleForm();
             submitTask();
             cancelTask();
         });
@@ -48,6 +47,14 @@ const Task = () => {
         });
     };
 
+    const setDate = (e) => {
+        let form = document.createElement("input");
+        form.setAttribute("type", "date");
+        form.setAttribute("min", "2018-01-01");
+        form.style.marginLeft = "60%";
+        e.target.replaceWith(form);
+    };
+
     const taskToList = function () {
         let taskContainer = document.querySelector("#task-container");
         let input = document.querySelector("#task-input");
@@ -57,23 +64,36 @@ const Task = () => {
         task.textContent = input.value;
         let taskIcon = document.createElement("i");
         taskIcon.setAttribute("class", "far fa-circle");
+        let date = document.createElement("div");
+        date.textContent = "No Date";
+        date.style.fontSize = "15px";
+        date.style.marginLeft = "70%";
+        let cancel = document.createElement("i");
+        cancel.setAttribute("class", "fas fa-xmark");
+        // cancel.style.fontSize = "15px";
+
+        // add event listener to allow date setting
+        date.addEventListener("click", (e) => {
+            setDate(e);
+        });
 
         // TODO: style circle
         taskIcon.style.fontSize = "20px";
         taskIcon.style.border;
 
         // style container
-        container.append(taskIcon, task);
+        container.append(taskIcon, task, date, cancel);
         container.style.display = "flex";
         container.style.gap = "15px";
         container.style.marginTop = "15px";
-        container.style.marginBottom = "10px";
+        container.style.marginBottom = "15px";
 
         // display task list
         taskContainer.before(container);
 
         // display add task tab again once task is added
         taskContainer.style.display = "flex";
+
         // toggle form to hide
         document.querySelector("#form-container").style.display = "None";
     };
