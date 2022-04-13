d2a281fd7c74c724ee8578d2270c5d0cd5e5ca94
Daniel Lee <daniel.lee@circleci.com>
2022-04-12 23:17:27 -0700

    components

---
+++ cv-template/src/compoenents/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import App from '../App';
+
+const Header = () => {
+    return (
+        <div className = "bg-[#7b9acc] h-20">
+            <h1 className = "text-[#FCF6F5] text-2xl pt-5 pl-3">
+                Create you CV!
+            </h1>
+        </div>
+    )
+}
+
+
+export default Header;
