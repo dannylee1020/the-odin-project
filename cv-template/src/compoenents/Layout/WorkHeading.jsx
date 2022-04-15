5be1d698c221cc5daa895dd446d753d2a70fe910
Daniel Lee <daniel.lee@circleci.com>
2022-04-14 17:27:33 -0700

    components for layout

---
+++ cv-template/src/compoenents/Layout/WorkHeading.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+const WorkHeading = () => {
+    return (
+        <div>
+            <h3 className = 'text-xl font-bold mb-1'>Work Experience</h3>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default WorkHeading;
