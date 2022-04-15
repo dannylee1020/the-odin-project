5feaf794e8ab744b17c6168256c57b8c0a1dc270
Daniel Lee <daniel.lee@circleci.com>
2022-04-14 17:30:06 -0700

    for adding individual work experience

---
+++ cv-template/src/compoenents/WorkExp/WorkExp.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class WorkExp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {
+            jobTitle,
+            companyTitle,
+            date,
+            location,
+            description
+        } = this.props.work
+
+
+        return (
+            <div>
+                <h4>{jobTitle}</h4>
+                <p>{companyTitle}</p>
+                <p>{date}</p>
+                <p>{location}</p>
+                <p>{description}</p>
+            </div>
+        )
+    }
+
+}
+
+export default WorkExp;
