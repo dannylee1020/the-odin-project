946f8d97d9b8e59d7d136108f950891e33e3493f
Daniel Lee <daniel.lee@circleci.com>
2022-04-20 22:43:08 -0700

    heading for education section

---
+++ cv-template/src/components/layout/EduHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EduHeading = () => {
+    return (
+        <div className='mt-10'>
+            <h3 className='text-xl font-bold mb-1'>Education</h3>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default EduHeading
