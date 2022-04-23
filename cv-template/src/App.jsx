83fb2c9bf472bf021a43f69a2cad35fe93dc48c0
Daniel Lee <daniel.lee@circleci.com>
2022-04-23 15:38:15 -0700

    add export button

--- cv-template/src/App.jsx
+++ cv-template/src/App.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header'
-import PersonalInfo from './components/PersonalInfo'
+import PersonalInfo from './components/personal/PersonalInfo'
 import WorkHeading from './components/layout/WorkHeading'
 import WorkHist from './components/workexp/WorkHist'
-import EduHist from './components/education/EduHist';
+import EduHist from './components/education/EduHist'
+import ExportBttn from './components/layout/ExportBttn'
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.elementRef = React.createRef();
     }
 
     render() {
         return (
             <div>
                 <Header></Header>
-                <PersonalInfo></PersonalInfo>
-                <WorkHeading></WorkHeading>
-                <WorkHist></WorkHist>
-                <EduHist></EduHist>
+                <div className='mt-20 flex flex-row justify-evenly'>
+                    <div className='ml-3/4'></div>
+                    <ExportBttn targetEl={this.elementRef}></ExportBttn>
+                </div>
+                <div className='content' ref={this.elementRef}>
+                    <PersonalInfo></PersonalInfo>
+                    <WorkHeading></WorkHeading>
+                    <WorkHist></WorkHist>
+                    <EduHist></EduHist>
+                </div>
             </div>
         )
     }
