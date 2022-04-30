e10629b8f60f8855e04328c514dc355aab80191f
Daniel Lee <daniel.lee@circleci.com>
2022-04-30 14:56:35 -0700

    refactor to functions

--- cv-template/src/App.jsx
+++ cv-template/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react'
 import Header from './components/layout/Header'
 import PersonalInfo from './components/personal/PersonalInfo'
 import WorkHeading from './components/layout/WorkHeading'
@@ -6,28 +6,24 @@ import WorkHist from './components/workexp/WorkHist'
 import EduHist from './components/education/EduHist'
 import ExportBttn from './components/layout/ExportBttn'
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.elementRef = React.createRef();
-    }
 
-    render() {
-        return (
-            <div>
-                <Header></Header>
-                <div className='mt-20 ml-[70%]'>
-                    <ExportBttn targetEl={this.elementRef}></ExportBttn>
-                </div>
-                <div className='content ml-[25%] ' ref={this.elementRef}>
-                    <PersonalInfo></PersonalInfo>
-                    <WorkHeading></WorkHeading>
-                    <WorkHist></WorkHist>
-                    <EduHist></EduHist>
-                </div>
+const App = () => {
+    const elementRef = useRef()
+
+    return (
+        <div>
+            <Header></Header>
+            <div className='mt-20 ml-[70%]'>
+                <ExportBttn targetEl={elementRef}></ExportBttn>
             </div>
-        )
-    }
+            <div className='content ml-[25%] ' ref={elementRef}>
+                <PersonalInfo></PersonalInfo>
+                <WorkHeading></WorkHeading>
+                <WorkHist></WorkHist>
+                <EduHist></EduHist>
+            </div>
+        </div>
+    )
 }
 
 export default App;
