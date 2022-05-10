513d8a01e491215e27ff7d4a0142a5b9556c51af
Daniel Lee <daniel.lee@circleci.com>
2022-05-09 21:37:09 -0700

    component for running the app

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,5 +1,35 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Header from  './components/layout/Header'
+import Card from './components/game-manager/Card'
 
-function App() {}
+function App () {
+
+    const [curScore, setCurScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    const incrementScore = () => {
+        setCurScore(curScore + 1)
+    }
+
+    const compareScore = () => {
+        if (curScore > bestScore) {
+            setBestScore(curScore)
+        } else {
+            return
+        }
+    }
+
+    const resetScore = () => {
+        setCurScore(0);
+    }
+
+
+    return (
+        <div className='bg-[#10182A] h-full'>
+            <Header curScore={curScore} bestScore={bestScore}></Header>
+            <Card incrementScore = {incrementScore} compareScore = {compareScore} resetScore={resetScore}></Card>
+        </div>
+    )
+}
+
+export default App
