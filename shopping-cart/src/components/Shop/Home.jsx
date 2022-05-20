983896b57363abc99b0d3b0ec948a9d81c168de2
Daniel Lee <daniel.lee@circleci.com>
2022-05-19 22:41:22 -0700

    adding other compoenents for making shop page

--- shopping-cart/src/components/Shop/Home.jsx
+++ shopping-cart/src/components/Shop/Home.jsx
@@ -1,11 +1,16 @@
 import Product from './Product';
 import Header from './Header';
+import ShowModal from './Modal';
+import { useDisclosure } from '@chakra-ui/react';
 
 const Home = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <div>
             <Header></Header>
-            <Product></Product>
+            <Product onOpen={onOpen}></Product>
+            <ShowModal isOpen={isOpen} onClose={onClose}></ShowModal>
         </div>
     )
 }
