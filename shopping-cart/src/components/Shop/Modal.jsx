3b99965e420f3330fa11edafb4aaa9f144c0e9ff
Daniel Lee <daniel.lee@circleci.com>
2022-05-21 15:03:31 -0700

    work on layouot of the drawer

--- shopping-cart/src/components/Shop/Modal.jsx
+++ shopping-cart/src/components/Shop/Modal.jsx
@@ -1,54 +1,61 @@
 import {
-    Modal,
-    ModalContent,
-    ModalOverlay,
-    ModalHeader,
-    ModalBody,
-    ModalCloseButton,
     Container,
+    Drawer,
+    DrawerContent,
+    DrawerOverlay,
+    DrawerHeader,
+    DrawerBody,
+    DrawerCloseButton,
     Flex,
-    Heading,
     VStack,
     HStack,
     Text,
+    Heading,
     Icon,
     Image,
-    Divider
+    Divider,
+    Spacer,
+    Button
 } from '@chakra-ui/react'
 
 import { HiMinusSm, HiPlusSm  } from 'react-icons/hi';
+import mini from '../../assets/air_mini.png'
 
-
-//! use Drawer component instead of Modal
 
 const ShowModal = (props) => {
 
     return (
-        <Modal isOpen={props.isOpen} onClose={props.onClose} motionPreset='slideInRight'>
-            <ModalOverlay/>
-            <ModalContent>
-                <ModalHeader>Your cart</ModalHeader>
-                <ModalCloseButton/>
-                <ModalBody>
-                    <VStack>
-                        <Heading as='h4'>The clean air you deserve.</Heading>
+        <Drawer isOpen={props.isOpen} onClose={props.onClose} motionPreset='slideInRight' size='md'>
+            <DrawerOverlay/>
+            <DrawerContent>
+                <DrawerHeader>Your cart</DrawerHeader>
+                <DrawerCloseButton/>
+                <DrawerBody>
+                    <VStack align='left' spacing={10}>
+                        <Heading size='md'>The clean air you deserve.</Heading>
                         <Flex>
-                            <Image src='' alt='placeholder'></Image>
-                            <Flex direction='column' justify='left'>
-                                <Heading as='h4'>Molekule Air Mini+</Heading>
+                            <Image src={mini} alt='placeholder' boxSize='100px' pr={5}></Image>
+                            <Flex direction='column' align='center'>
+                                <Text fontSize='lg'>Molekule Air Mini+</Text>
                                 <HStack>
                                     <Icon as={HiMinusSm}></Icon>
                                     <Text>1</Text>
                                     <Icon as={HiPlusSm}></Icon>
                                 </HStack>
                             </Flex>
+                            <Spacer/>
                             <Text>$499.99</Text>
                         </Flex>
                         <Divider/>
+                        <Flex justify='space-between'>
+                                <Text fontWeight='bold'>Total</Text>
+                                <Text fontWeight='bold'>$499.99</Text>
+                            </Flex>
+                        <Button colorScheme='teal'>Checkout</Button>
                     </VStack>
-                </ModalBody>
-            </ModalContent>
-        </Modal>
+                </DrawerBody>
+            </DrawerContent>
+        </Drawer>
     )
 
 }
