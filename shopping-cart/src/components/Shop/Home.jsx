3135262be09c34e1b674bc40e7bd914b70a83ac0
Daniel Lee <daniel.lee@circleci.com>
2022-05-22 23:09:42 -0700

    function to dynamically render DOM

--- shopping-cart/src/components/Shop/Home.jsx
+++ shopping-cart/src/components/Shop/Home.jsx
@@ -2,15 +2,41 @@ import Product from './Product';
 import Header from './Header';
 import ShowModal from './Modal';
 import { useDisclosure } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+
+import mini from '../../assets/air_mini.png'
+import pro from '../../assets/air_pro.png'
 
 const Home = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [product, setProduct] = useState({});
+
+    const handleProduct = (e) => {
+        let productData = {}
+
+        if (e.currentTarget.id === 'air-mini') {
+            productData = {
+                name: 'Molekule Air Mini+',
+                src: mini,
+                price: '$499.99'
+            }
+        } else {
+            productData = {
+                name: 'Molekule Air Pro',
+                src: pro,
+                price: '$999.99'
+            }
+        }
+
+        setProduct(productData)
+    }
+
 
     return (
         <div>
             <Header></Header>
-            <Product onOpen={onOpen}></Product>
-            <ShowModal isOpen={isOpen} onClose={onClose}></ShowModal>
+            <Product onOpen={onOpen} handleProduct={handleProduct}></Product>
+            <ShowModal isOpen={isOpen} onClose={onClose} product={product}></ShowModal>
         </div>
     )
 }
