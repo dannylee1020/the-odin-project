789df6b5dbf08148515a2e22ede09562698d08ae
Daniel Lee <daniel.lee@circleci.com>
2022-05-24 22:41:20 -0700

    receive props from parent

--- shopping-cart/src/components/Shop/Modal.jsx
+++ shopping-cart/src/components/Shop/Modal.jsx
@@ -14,63 +14,69 @@ import {
     Image,
     Divider,
     Spacer,
-    Button
-} from '@chakra-ui/react'
+    Button,
+} from "@chakra-ui/react";
 
-import { HiMinusSm, HiPlusSm  } from 'react-icons/hi';
-import { useState } from 'react';
+import { HiMinusSm, HiPlusSm } from "react-icons/hi";
 
 const ShowModal = (props) => {
-
-    const {src, name, price} = props.product;
-    const [quantity, setQuantity] = useState(1);
-
-    const handleQuantity = (e) => {
-        if (e.currentTarget.id === 'subtract') {
-            setQuantity(quantity - 1)
-        } else if (e.currentTarget.id === 'add') {
-            setQuantity(quantity + 1)
-        } else {
-            return
-        }
-    }
-
-
+    const { src, name, price, quantity } = props.product;
 
     return (
-        <Drawer isOpen={props.isOpen} onClose={props.onClose} motionPreset='slideInRight' size='md'>
-            <DrawerOverlay/>
+        <Drawer
+            isOpen={props.isOpen}
+            onClose={props.onClose}
+            motionPreset="slideInRight"
+            size="md"
+        >
+            <DrawerOverlay />
             <DrawerContent>
                 <DrawerHeader>Your cart</DrawerHeader>
-                <DrawerCloseButton/>
+                <DrawerCloseButton />
                 <DrawerBody>
-                    <VStack align='left' spacing={10}>
-                        <Heading size='md'>The clean air you deserve.</Heading>
+                    <VStack align="left" spacing={10}>
+                        <Heading size="md">The clean air you deserve.</Heading>
                         <Flex>
-                            <Image src={src} alt='placeholder' boxSize='100px' pr={5}></Image>
-                            <Flex direction='column' align='center'>
-                                <Text fontSize='lg'>{name}</Text>
+                            <Image
+                                src={src}
+                                alt="placeholder"
+                                boxSize="100px"
+                                pr={5}
+                            ></Image>
+                            <Flex direction="column" align="center">
+                                <Text fontSize="lg">{name}</Text>
                                 <HStack>
-                                    <Icon as={HiMinusSm} id='subtract' onClick={handleQuantity}></Icon>
+                                    <Icon
+                                        as={HiMinusSm}
+                                        id="subtract"
+                                        onClick={(e) => {
+                                            props.handleQuantity(e);
+                                        }}
+                                    ></Icon>
                                     <Text>{quantity}</Text>
-                                    <Icon as={HiPlusSm} id='add' onClick={handleQuantity}></Icon>
+                                    <Icon
+                                        as={HiPlusSm}
+                                        id="add"
+                                        onClick={(e) => {
+                                            props.handleQuantity(e);
+                                        }}
+                                    ></Icon>
                                 </HStack>
                             </Flex>
-                            <Spacer/>
+                            <Spacer />
                             <Text>{price}</Text>
                         </Flex>
-                        <Divider/>
-                        <Flex justify='space-between'>
-                                <Text fontWeight='bold'>Total</Text>
-                                <Text fontWeight='bold'>{parseInt({price}) * parseInt({quantity})}</Text>
-                            </Flex>
-                        <Button colorScheme='teal'>Checkout</Button>
+                        <Divider />
+                        <Flex justify="space-between">
+                            <Text fontWeight="bold">Total</Text>
+                            <Text fontWeight="bold">${price * quantity}</Text>
+                        </Flex>
+                        <Button colorScheme="teal">Checkout</Button>
                     </VStack>
                 </DrawerBody>
             </DrawerContent>
         </Drawer>
-    )
-
-}
+    );
+};
 
 export default ShowModal;
