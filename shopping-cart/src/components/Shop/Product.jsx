ad0494a34ffc12fe6587a5b7813438e95bc5dbda
Daniel Lee <daniel.lee@circleci.com>
2022-05-24 22:41:55 -0700

    receive props to handle add to cart functionality

--- shopping-cart/src/components/Shop/Product.jsx
+++ shopping-cart/src/components/Shop/Product.jsx
@@ -1,60 +1,73 @@
-import { Container, Flex, VStack, Image, Text, Heading, Button } from '@chakra-ui/react'
-import mini from '../../assets/air_mini.png'
-import pro from '../../assets/air_pro.png'
+import {
+    Container,
+    Flex,
+    VStack,
+    Image,
+    Text,
+    Heading,
+    Button,
+} from "@chakra-ui/react";
+import mini from "../../assets/air_mini.png";
+import pro from "../../assets/air_pro.png";
 
 const Home = (props) => {
     return (
-        <Container maxW='container.lg' p={0}>
-            <Flex maxH='80%' mt={40} align='flex-end' justify='center' gap={40}>
+        <Container maxW="container.lg" p={0}>
+            <Flex maxH="80%" mt={40} align="flex-end" justify="center" gap={40}>
                 <VStack>
                     <Image src={mini} />
-                    <Heading as='h4' size='lg'>
+                    <Heading as="h4" size="lg">
                         Air Mini+
                     </Heading>
-                    <Text align='center'>
-                        For small rooms up to 250 sq ft
-                    </Text>
-                    <Text align='center'>
-                        Auto Protect mode
-                    </Text>
-                    <Text align='center'>
-                        Particle sensor
-                    </Text>
-                    <Text fontSize='xl' fontWeight='bold' py={5}>
+                    <Text align="center">For small rooms up to 250 sq ft</Text>
+                    <Text align="center">Auto Protect mode</Text>
+                    <Text align="center">Particle sensor</Text>
+                    <Text fontSize="xl" fontWeight="bold" py={5}>
                         $42/Month | $499.99
                     </Text>
-                    <Button colorScheme='gray' size='md' borderRadius='5px' id='air-mini' onClick={(e) => {props.onOpen(); props.handleProduct(e)}}>
+                    <Button
+                        colorScheme="gray"
+                        size="md"
+                        borderRadius="5px"
+                        id="air-mini"
+                        onClick={(e) => {
+                            props.onOpen();
+                            props.handleProduct(e);
+                        }}
+                    >
                         Add to cart
                     </Button>
                 </VStack>
                 <VStack>
                     <Image src={pro} />
-                    <Heading as='h4' size='lg'>
+                    <Heading as="h4" size="lg">
                         Air Pro+
                     </Heading>
-                    <Text align='center'>
-                        Day View
-                    </Text>
-                    <Text align='center'>
+                    <Text align="center">Day View</Text>
+                    <Text align="center">
                         Senses Chemicals (VOCs), humidity, CO2
                     </Text>
-                    <Text align='center'>
-                        Auto Protect reacts to VOCs
-                    </Text>
-                    <Text align='center'>
-                        For spaces up to 1000 sq ft
-                    </Text>
-                    <Text fontSize='xl' fontWeight='bold' py={5}>
+                    <Text align="center">Auto Protect reacts to VOCs</Text>
+                    <Text align="center">For spaces up to 1000 sq ft</Text>
+                    <Text fontSize="xl" fontWeight="bold" py={5}>
                         $63/Month | $999.99
                     </Text>
-                    <Button colorScheme='gray' size='md' borderRadius='5px' id='air-pro' onClick={(e) => {props.onOpen(); props.handleProduct(e)}}>
+                    <Button
+                        colorScheme="gray"
+                        size="md"
+                        borderRadius="5px"
+                        id="air-pro"
+                        onClick={(e) => {
+                            props.onOpen();
+                            props.handleProduct(e);
+                        }}
+                    >
                         Add to cart
                     </Button>
                 </VStack>
             </Flex>
         </Container>
-    )
+    );
+};
 
-}
-
-export default Home
+export default Home;
