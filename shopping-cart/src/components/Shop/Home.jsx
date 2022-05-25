79121b37c750d1d494127ba7eaeefb96d27c8c32
Daniel Lee <daniel.lee@circleci.com>
2022-05-24 22:41:11 -0700

    adding fucntions to handle quantity changes and total calculation

--- shopping-cart/src/components/Shop/Home.jsx
+++ shopping-cart/src/components/Shop/Home.jsx
@@ -1,44 +1,66 @@
-import Product from './Product';
-import Header from './Header';
-import ShowModal from './Modal';
-import { useDisclosure } from '@chakra-ui/react';
-import { useState, useEffect } from 'react';
+import Product from "./Product";
+import Header from "./Header";
+import ShowModal from "./Modal";
+import { useDisclosure } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
-import mini from '../../assets/air_mini.png'
-import pro from '../../assets/air_pro.png'
+import mini from "../../assets/air_mini.png";
+import pro from "../../assets/air_pro.png";
+
+//* prevent negative quantity
+//* format float points
+//* make add to cart to add quantity to the cart
 
 const Home = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [product, setProduct] = useState({});
 
     const handleProduct = (e) => {
-        let productData = {}
-
-        if (e.currentTarget.id === 'air-mini') {
-            productData = {
-                name: 'Molekule Air Mini+',
+        if (e.currentTarget.id === "air-mini") {
+            setProduct({
+                name: "Molekule Air Mini+",
                 src: mini,
-                price: '$499.99'
-            }
+                price: 499.99,
+                quantity: 1,
+            });
         } else {
-            productData = {
-                name: 'Molekule Air Pro',
+            setProduct({
+                name: "Molekule Air Pro",
                 src: pro,
-                price: '$999.99'
-            }
+                price: 999.99,
+                quantity: 1,
+            });
         }
+    };
 
-        setProduct(productData)
-    }
-
+    const handleQuantity = (e) => {
+        if (e.currentTarget.id === "subtract") {
+            setProduct((prev) => ({
+                ...prev,
+                quantity: prev.quantity - 1,
+            }));
+        } else if (e.currentTarget.id === "add") {
+            setProduct((prev) => ({
+                ...prev,
+                quantity: prev.quantity + 1,
+            }));
+        } else {
+            return;
+        }
+    };
 
     return (
         <div>
             <Header></Header>
             <Product onOpen={onOpen} handleProduct={handleProduct}></Product>
-            <ShowModal isOpen={isOpen} onClose={onClose} product={product}></ShowModal>
+            <ShowModal
+                isOpen={isOpen}
+                onClose={onClose}
+                product={product}
+                handleQuantity={handleQuantity}
+            ></ShowModal>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
