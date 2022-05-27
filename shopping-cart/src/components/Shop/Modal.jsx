943ceaf28e503d7ce9fd2657cbb2bf5c3e648228
Daniel Lee <daniel.lee@circleci.com>
2022-05-26 21:23:46 -0700

    conditionally render component

--- shopping-cart/src/components/Shop/Modal.jsx
+++ shopping-cart/src/components/Shop/Modal.jsx
@@ -1,4 +1,5 @@
 import {
+    Container,
     Drawer,
     DrawerContent,
     DrawerOverlay,
@@ -20,7 +21,83 @@ import {
 import { HiMinusSm, HiPlusSm } from "react-icons/hi";
 
 const ShowModal = (props) => {
-    const { src, name, price, quantity } = props.product;
+    const displayMini = () => {
+        if (!props.mini.displayMini) {
+            return;
+        }
+
+        return (
+            <Container>
+                <Image
+                    src={props.mini.src}
+                    alt="placeholder"
+                    boxSize="100px"
+                    pr={5}
+                ></Image>
+                <Flex direction="column" align="center">
+                    <Text fontSize="lg">{props.mini.name}</Text>
+                    <HStack>
+                        <Icon
+                            as={HiMinusSm}
+                            id="subtract"
+                            onClick={(e) => {
+                                props.handleQuantity(e);
+                            }}
+                        ></Icon>
+                        <Text>{props.mini.quantity}</Text>
+                        <Icon
+                            as={HiPlusSm}
+                            id="add"
+                            onClick={(e) => {
+                                props.handleQuantity(e);
+                            }}
+                        ></Icon>
+                    </HStack>
+                </Flex>
+                <Spacer />
+                <Text>{props.mini.price}</Text>
+            </Container>
+        );
+    };
+
+    const displayPro = () => {
+        if (!props.pro.displayPro) {
+            return;
+        }
+
+        return (
+            <Container>
+                <Image
+                    src={props.pro.src}
+                    alt="placeholder"
+                    boxSize="100px"
+                    pr={5}
+                ></Image>
+                <Flex direction="column" align="center">
+                    <Text fontSize="lg">{props.pro.name}</Text>
+                    <HStack>
+                        <Icon
+                            as={HiMinusSm}
+                            id="subtract"
+                            onClick={(e) => {
+                                props.handleQuantity(e);
+                            }}
+                        ></Icon>
+                        <Text>{props.pro.quantity}</Text>
+                        <Icon
+                            as={HiPlusSm}
+                            id="add"
+                            onClick={(e) => {
+                                props.handleQuantity(e);
+                            }}
+                        ></Icon>
+                    </HStack>
+                </Flex>
+                <Spacer />
+                <Text>{props.pro.price}</Text>
+            </Container>
+        );
+    };
 
     return (
         <Drawer
@@ -37,39 +114,15 @@ const ShowModal = (props) => {
                     <VStack align="left" spacing={10}>
                         <Heading size="md">The clean air you deserve.</Heading>
                         <Flex>
-                            <Image
-                                src={src}
-                                alt="placeholder"
-                                boxSize="100px"
-                                pr={5}
-                            ></Image>
-                            <Flex direction="column" align="center">
-                                <Text fontSize="lg">{name}</Text>
-                                <HStack>
-                                    <Icon
-                                        as={HiMinusSm}
-                                        id="subtract"
-                                        onClick={(e) => {
-                                            props.handleQuantity(e);
-                                        }}
-                                    ></Icon>
-                                    <Text>{quantity}</Text>
-                                    <Icon
-                                        as={HiPlusSm}
-                                        id="add"
-                                        onClick={(e) => {
-                                            props.handleQuantity(e);
-                                        }}
-                                    ></Icon>
-                                </HStack>
-                            </Flex>
-                            <Spacer />
-                            <Text>{price}</Text>
+                            {displayMini()}
+                            {displayPro()}
                         </Flex>
                         <Divider />
                         <Flex justify="space-between">
                             <Text fontWeight="bold">Total</Text>
-                            <Text fontWeight="bold">${price * quantity}</Text>
+                            <Text fontWeight="bold">
+                                {/* ${(price * quantity).toFixed(2)} */}
+                            </Text>
                         </Flex>
                         <Button colorScheme="teal">Checkout</Button>
                     </VStack>
