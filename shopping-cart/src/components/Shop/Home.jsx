eaef5d6bbe6751dd5b27e9e71020a7f0b244df7a
Daniel Lee <daniel.lee@circleci.com>
2022-05-26 21:23:30 -0700

    handle states for each product differently

--- shopping-cart/src/components/Shop/Home.jsx
+++ shopping-cart/src/components/Shop/Home.jsx
@@ -4,32 +4,34 @@ import ShowModal from "./Modal";
 import { useDisclosure } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
-import mini from "../../assets/air_mini.png";
-import pro from "../../assets/air_pro.png";
+import assetMini from "../../assets/air_mini.png";
+import assetPro from "../../assets/air_pro.png";
 
-//* prevent negative quantity
-//* format float points
-//* make add to cart to add quantity to the cart
+//! cart should be empty when first rendered
+//! fix layout of the cart
 
 const Home = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [product, setProduct] = useState({});
+    const [pro, setPro] = useState({ quantity: 0, displayPro: false });
+    const [mini, setMini] = useState({ quantity: 0, displayMini: false });
 
     const handleProduct = (e) => {
         if (e.currentTarget.id === "air-mini") {
-            setProduct({
+            setMini((prev) => ({
                 name: "Molekule Air Mini+",
-                src: mini,
+                src: assetMini,
                 price: 499.99,
-                quantity: 1,
-            });
+                quantity: prev.quantity + 1,
+                displayMini: true,
+            }));
         } else {
-            setProduct({
+            setPro((prev) => ({
                 name: "Molekule Air Pro",
-                src: pro,
+                src: assetPro,
                 price: 999.99,
-                quantity: 1,
-            });
+                quantity: prev.quantity + 1,
+                displayPro: true,
+            }));
         }
     };
 
@@ -37,12 +39,12 @@ const Home = () => {
         if (e.currentTarget.id === "subtract") {
             setProduct((prev) => ({
                 ...prev,
-                quantity: prev.quantity - 1,
+                quantity: Math.max(prev.quantity - 1, 0),
             }));
         } else if (e.currentTarget.id === "add") {
             setProduct((prev) => ({
                 ...prev,
-                quantity: prev.quantity + 1,
+                quantity: Math.max(prev.quantity + 1, 0),
             }));
         } else {
             return;
@@ -56,7 +58,8 @@ const Home = () => {
             <ShowModal
                 isOpen={isOpen}
                 onClose={onClose}
-                product={product}
+                pro={pro}
+                mini={mini}
                 handleQuantity={handleQuantity}
             ></ShowModal>
         </div>
