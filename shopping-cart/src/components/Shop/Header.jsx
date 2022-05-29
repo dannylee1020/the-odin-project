7a1e89ecf4d85604b5eb3f5f2e174df3f8e39dbc
Daniel Lee <daniel.lee@circleci.com>
2022-05-29 14:48:36 -0700

    make drawer pop up on cart click

--- shopping-cart/src/components/Shop/Header.jsx
+++ shopping-cart/src/components/Shop/Header.jsx
@@ -1,19 +1,26 @@
-import { Container, Flex, Divider, Icon, Spacer, Image} from '@chakra-ui/react';
-import { BiShoppingBag } from 'react-icons/bi';
-import logo from '../../assets/logo.png'
+import {
+    Container,
+    Flex,
+    Divider,
+    Icon,
+    Spacer,
+    Image,
+} from "@chakra-ui/react";
+import { BiShoppingBag } from "react-icons/bi";
+import logo from "../../assets/logo.png";
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <Container maxW='100%'>
-            <Flex align='center' justify='flex-end'>
-                <Spacer/>
-                <Image src={logo} h={50}/>
-                <Spacer/>
-                <Icon as={BiShoppingBag} w={8} h={6}/>
+        <Container maxW="100%">
+            <Flex align="center" justify="flex-end">
+                <Spacer />
+                <Image src={logo} h={50} />
+                <Spacer />
+                <Icon as={BiShoppingBag} w={8} h={6} onClick={props.onOpen} />
             </Flex>
-            <Divider/>
+            <Divider />
         </Container>
-    )
-}
+    );
+};
 
 export default Header;
