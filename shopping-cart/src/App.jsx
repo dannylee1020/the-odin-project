80db6f286fd12307ca695b1777fc88126a85fcaa
Daniel Lee <daniel.lee@circleci.com>
2022-06-01 21:49:04 -0700

    adding checkout component and rerouter"

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import Home from './components/Shop/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Shop/Home";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
     return (
-        <Home/>
-    )
-}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<Cart />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
