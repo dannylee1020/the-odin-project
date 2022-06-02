dd3ffb2ec33274b564e228c46b681a36ba8f461c
Daniel Lee <daniel.lee@circleci.com>
2022-06-01 22:36:19 -0700

    add header

--- shopping-cart/src/components/Cart/Cart.jsx
+++ shopping-cart/src/components/Cart/Cart.jsx
@@ -1,15 +1,51 @@
 import Shipping from "./Shipping";
 import OrderSummary from "./OrderSummary";
-import { Box, VStack } from "@chakra-ui/react";
+import {
+    Box,
+    VStack,
+    Container,
+    Flex,
+    Image,
+    Spacer,
+    Icon,
+} from "@chakra-ui/react";
+import { BiHomeAlt } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Cart = () => {
+    const navigate = useNavigate();
+
     return (
-        <Box bg="#F4F2F2" w="100%" maxH="100%">
-            <VStack spacing={10} align="center" pb="20">
-                <Shipping></Shipping>
-                <OrderSummary></OrderSummary>
-            </VStack>
-        </Box>
+        <div>
+            <Container maxW="100%">
+                <Flex align="center" justify="flex-end">
+                    <Spacer />
+                    <Image
+                        src={logo}
+                        h={50}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    />
+                    <Spacer />
+                    <Icon
+                        as={BiHomeAlt}
+                        w={8}
+                        h={6}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    ></Icon>
+                </Flex>
+            </Container>
+            <Box bg="#F4F2F2" w="100%" maxH="100%">
+                <VStack spacing={10} align="center" pb="20">
+                    <Shipping></Shipping>
+                    <OrderSummary></OrderSummary>
+                </VStack>
+            </Box>
+        </div>
     );
 };
 
