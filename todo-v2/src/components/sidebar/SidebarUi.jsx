e324dd06c582c33016512200696cddc03fe9df18
Daniel Lee <daniel.lee@circleci.com>
2022-06-11 16:10:53 -0700

    component for sidebar ui

---
+++ todo-v2/src/components/sidebar/SidebarUi.jsx
@@ -0,0 +1,57 @@
+import { MdToday } from "react-icons/md";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { GrProjects } from "react-icons/gr";
+import { BsInbox } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
+const SidebarUi = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="w-72 h-screen flex flex-col bg-[#FAFAFA] pl-10">
+            <div className="flex flex-col items-left mt-12 gap-4">
+                <div
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                >
+                    <span>
+                        <BsInbox />
+                    </span>
+                    Inbox
+                </div>
+                <div
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        navigate("/today");
+                    }}
+                >
+                    <span>
+                        <MdToday />
+                    </span>
+                    Today
+                </div>
+                <div
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        navigate("/upcoming");
+                    }}
+                >
+                    <span>
+                        <AiOutlineCalendar />
+                    </span>
+                    Upcoming
+                </div>
+                <div className="flex items-center gap-2">
+                    <span>
+                        <GrProjects />
+                    </span>
+                    Projects
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarUi;
