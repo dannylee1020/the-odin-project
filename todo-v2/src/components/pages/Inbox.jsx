0b9eedf9749cdae5ade53a13b823834a4d4d61c4
Daniel Lee <daniel.lee@circleci.com>
2022-06-12 14:14:41 -0700

    ui for inbox

--- todo-v2/src/components/pages/Inbox.jsx
+++ todo-v2/src/components/pages/Inbox.jsx
@@ -1,15 +1,16 @@
 import { BsPlus } from "react-icons/bs";
 
-const Inbox = () => {
+const Inbox = (props) => {
     return (
         <div>
             <h2 className="font-bold">Inbox</h2>
-            <div className="flex items-center">
+            <div className="flex items-center" onClick={props.isOpen}>
                 <span>
                     <BsPlus />
                 </span>
                 Add Task
             </div>
+            <div>{props.makeModal()}</div>
         </div>
     );
 };
