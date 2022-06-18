d733ff9a3e9e31e50be589bb51676084bb946a6a
Daniel Lee <daniel.lee@circleci.com>
2022-06-18 14:57:35 -0700

    add component to render tasks

--- todo-v2/src/components/pages/Inbox.jsx
+++ todo-v2/src/components/pages/Inbox.jsx
@@ -1,16 +1,20 @@
 import { BsPlus } from "react-icons/bs";
+import TaskManager from "./TaskManager";
 
 const Inbox = (props) => {
     return (
-        <div>
-            <h2 className="font-bold">Inbox</h2>
-            <div className="flex items-center" onClick={props.isOpen}>
-                <span>
-                    <BsPlus />
-                </span>
-                Add Task
+        <div className="w-screen flex flex-col">
+            <div>
+                <h2 className="font-bold">Inbox</h2>
+                <div className="flex items-center" onClick={props.isOpen}>
+                    <span>
+                        <BsPlus />
+                    </span>
+                    Add Task
+                </div>
+                <div>{props.makeModal()}</div>
             </div>
-            <div>{props.makeModal()}</div>
+            <TaskManager></TaskManager>
         </div>
     );
 };
