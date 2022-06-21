a555bb4a7f79138a2dbf76837ae1786574244150
Daniel Lee <daniel.lee@circleci.com>
2022-06-20 23:11:19 -0700

    remove function rendering modal

--- todo-v2/src/components/pages/Today.jsx
+++ todo-v2/src/components/pages/Today.jsx
@@ -1,16 +1,19 @@
 import { BsPlus } from "react-icons/bs";
+import TaskManager from "../tasks/TaskManager";
 
 const Today = (props) => {
     return (
-        <div>
-            <h2 className="font-bold">Today</h2>
-            <div className="flex items-center" onClick={props.isOpen}>
-                <span>
-                    <BsPlus />
-                </span>
-                Add Task
+        <div className="w-screen flex flex-col">
+            <div>
+                <h2 className="font-bold">Today</h2>
+                <div className="flex items-center" onClick={props.isOpen}>
+                    <span>
+                        <BsPlus />
+                    </span>
+                    Add Task
+                </div>
             </div>
-            <div>{props.makeModal()}</div>
+            <TaskManager></TaskManager>
         </div>
     );
 };
