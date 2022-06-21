4eb08f260321e8db0ff3283fecc38ebfc1fc5a95
Daniel Lee <daniel.lee@circleci.com>
2022-06-20 23:09:06 -0700

    separate out modal to a new component

--- todo-v2/src/App.jsx
+++ todo-v2/src/App.jsx
@@ -4,8 +4,8 @@ import SidebarUi from "./components/sidebar/SidebarUi";
 import Inbox from "./components/pages/Inbox";
 import Today from "./components/pages/Today";
 import Upcoming from "./components/pages/Upcoming";
-import { useState, useEffect } from "react";
-import { Dialog } from "@headlessui/react";
+import Modal from "./components/pages/Modal";
+import { useState } from "react";
 
 import { db } from "./firebase-config";
 import {
@@ -17,6 +17,8 @@ import {
     onSnapshot,
 } from "firebase/firestore";
 
+// TODO: add sign in functionality
+
 function App() {
     let [isOpen, setIsOpen] = useState(false);
     let [taskData, setTaskData] = useState([]);
@@ -27,51 +29,6 @@ function App() {
 
     const closeModal = () => {
         setIsOpen(false);
-    };
-
-    const makeModal = () => {
-        return (
-            <Dialog
-                open={isOpen}
-                onClose={closeModal}
-                className="absolute flex inset-0 items-center justify-center overflow-y-auto z-10"
-            >
-                <div className="fixed inset-0 bg-black bg-opacity-25" />
-                <Dialog.Panel className="w-full max-w-md transform rounded-2xl bg-white p-6 text-left align-middle shadow-xl">
-                    <div className="flex flex-col">
-                        <form onSubmit={handleSubmit}>
-                            <div className="flex flex-col border border-slate-300 rounded-md">
-                                <input
-                                    className="placeholder:italic border-t border-r border-l focus:outline-none pb-3 pl-1"
-                                    placeholder="Task"
-                                    name="title"
-                                    type="text"
-                                    required
-                                    pattern="\S(.*\S)?"
-                                ></input>
-                                <textarea
-                                    className="placeholder:italic w-100% h-28 focus:outline-none placeholder:text-sm pl-1"
-                                    placeholder="Description"
-                                    name="description"
-                                    required
-                                    pattern="\S(.*\S)?"
-                                    type="text"
-                                ></textarea>
-                                <input type="date" name="date" required></input>
-                            </div>
-                            <div className="flex gap-2 ml-2 mt-3">
-                                <input
-                                    type="submit"
-                                    value="Add"
-                                    onClick={queryTasks}
-                                ></input>
-                                <button onClick={closeModal}>Cancel</button>
-                            </div>
-                        </form>
-                    </div>
-                </Dialog.Panel>
-            </Dialog>
-        );
     };
 
     const writeToDB = async (data) => {
@@ -121,13 +78,29 @@ function App() {
                     <Route
                         path="/"
                         element={
-                            <Inbox makeModal={makeModal} isOpen={openModal} />
+                            <>
+                                <Inbox isOpen={openModal} />
+                                <Modal
+                                    handleSubmit={handleSubmit}
+                                    queryTasks={queryTasks}
+                                    closeModal={closeModal}
+                                    isOpen={isOpen}
+                                ></Modal>
+                            </>
                         }
                     ></Route>
                     <Route
                         path="/today"
                         element={
-                            <Today makeModal={makeModal} isOpen={openModal} />
+                            <>
+                                <Today isOpen={openModal} />
+                                <Modal
+                                    handleSubmit={handleSubmit}
+                                    queryTasks={queryTasks}
+                                    closeModal={closeModal}
+                                    isOpen={isOpen}
+                                ></Modal>
+                            </>
                         }
                     ></Route>
                     <Route path="/upcoming" element={<Upcoming />}></Route>
