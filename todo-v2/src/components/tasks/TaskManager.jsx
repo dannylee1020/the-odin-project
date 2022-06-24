678c5a5308c1d8a5a24416fd6d4f13b618435ce6
Daniel Lee <daniel.lee@circleci.com>
2022-06-23 23:41:14 -0700

    add logic to filter and render finished tasks to Done page

--- todo-v2/src/components/tasks/TaskManager.jsx
+++ todo-v2/src/components/tasks/TaskManager.jsx
@@ -4,10 +4,11 @@ import { db } from "../../firebase-config";
 import Tasks from "./Tasks";
 import { useLocation } from "react-router-dom";
 
-const TaskManager = (props) => {
+const TaskManager = () => {
     let [tasks, setTasks] = useState([]);
     let [today, setToday] = useState([]);
     let [upcoming, setUpcoming] = useState([]);
+    let [doneTasks, setDoneTasks] = useState([]);
 
     let location = useLocation();
 
@@ -18,22 +19,27 @@ const TaskManager = (props) => {
         .toISOString()
         .split("T")[0];
 
-    //TODO: add update and delete functionality
-
     useEffect(() => {
         const q = query(collection(db, "tasks"), orderBy("date", "asc"));
         onSnapshot(q, (qSnapshot) => {
             setTasks(
-                qSnapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    data: doc.data(),
-                }))
+                qSnapshot.docs
+                    .filter((doc) => {
+                        return doc.data().status.toLowerCase() !== "done";
+                    })
+                    .map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))
             );
 
             setToday(
                 qSnapshot.docs
                     .filter((doc) => {
-                        return doc.data().date === currentDate;
+                        return (
+                            doc.data().date === currentDate &&
+                            doc.data().status.toLowerCase() !== "done"
+                        );
                     })
                     .map((doc) => ({
                         id: doc.id,
@@ -44,7 +50,21 @@ const TaskManager = (props) => {
             setUpcoming(
                 qSnapshot.docs
                     .filter((doc) => {
-                        return doc.data().date > currentDate;
+                        return (
+                            doc.data().date > currentDate &&
+                            doc.data().status.toLowerCase() !== "done"
+                        );
+                    })
+                    .map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))
+            );
+
+            setDoneTasks(
+                qSnapshot.docs
+                    .filter((doc) => {
+                        return doc.data().status.toLowerCase() === "done";
                     })
                     .map((doc) => ({
                         id: doc.id,
@@ -61,6 +81,11 @@ const TaskManager = (props) => {
             });
         } else if (location.pathname === "/upcoming") {
             return upcoming.map((t) => {
+                return <Tasks task={t} key={t.id}></Tasks>;
+            });
+            // TODO: style done tasks
+        } else if (location.pathname === "/done") {
+            return doneTasks.map((t) => {
                 return <Tasks task={t} key={t.id}></Tasks>;
             });
         } else {
