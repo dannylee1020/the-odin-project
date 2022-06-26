4ea82491efa4293cce9dc603ef798aed71a7d2c6
Daniel Lee <daniel.lee@circleci.com>
2022-06-25 17:55:08 -0700

    form for updating task

---
+++ todo-v2/src/components/tasks/UpdateTask.jsx
@@ -0,0 +1,37 @@
+const UpdateTask = (props) => {
+    if (!props.isUpdate) {
+        return;
+    }
+
+    return (
+        <div className="flex flex-col">
+            <form onSubmit={props.handleUpdate}>
+                <div className="flex flex-col border border-slate-300 rounded-md">
+                    <input
+                        className="placeholder:italic border-t border-r border-l focus:outline-none pb-3 pl-1"
+                        placeholder="Task"
+                        name="title"
+                        type="text"
+                        required
+                        pattern="\S(.*\S)?"
+                    ></input>
+                    <textarea
+                        className="placeholder:italic w-100% h-28 focus:outline-none placeholder:text-sm pl-1"
+                        placeholder="Description"
+                        name="description"
+                        required
+                        pattern="\S(.*\S)?"
+                        type="text"
+                    ></textarea>
+                    <input type="date" name="date" required></input>
+                </div>
+                <div className="flex gap-2 ml-2 mt-3">
+                    <input type="submit" value="Update"></input>
+                    <button onClick={props.closeUpdate}>Cancel</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default UpdateTask;
