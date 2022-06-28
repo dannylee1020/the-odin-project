4edfbb24213c74eea86f0a8f87f90bd654ca551d
Daniel Lee <daniel.lee@circleci.com>
2022-06-27 22:19:58 -0700

    fix layout

--- todo-v2/src/components/pages/Today.jsx
+++ todo-v2/src/components/pages/Today.jsx
@@ -2,9 +2,9 @@ import { BsPlus } from "react-icons/bs";
 
 const Today = (props) => {
     return (
-        <div>
-            <h2 className="font-bold">Today</h2>
-            <div className="flex items-center" onClick={props.setAdd}>
+        <div className="mt-10">
+            <h2 className="font-bold text-xl mb-2">Today</h2>
+            <div className="flex items-center mb-5" onClick={props.setAdd}>
                 <span>
                     <BsPlus />
                 </span>
