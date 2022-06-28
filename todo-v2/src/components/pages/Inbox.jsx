d4135aa6db83cbd30f779be3d2aee67e71e03754
Daniel Lee <daniel.lee@circleci.com>
2022-06-27 22:19:22 -0700

    fix layout

--- todo-v2/src/components/pages/Inbox.jsx
+++ todo-v2/src/components/pages/Inbox.jsx
@@ -2,9 +2,9 @@ import { BsPlus } from "react-icons/bs";
 
 const Inbox = (props) => {
     return (
-        <div>
-            <h2 className="font-bold">Inbox</h2>
-            <div className="flex items-center" onClick={props.setAdd}>
+        <div className="mt-10">
+            <h2 className="font-bold text-xl mb-2">Inbox</h2>
+            <div className="flex items-center mb-5" onClick={props.setAdd}>
                 <span>
                     <BsPlus />
                 </span>
