de0a28272528b46b7f9535ce14da8ede837b9f4b
Daniel Lee <daniel.lee@circleci.com>
2022-06-27 22:20:41 -0700

    fix html form

--- todo-v2/src/components/tasks/UpdateTask.jsx
+++ todo-v2/src/components/tasks/UpdateTask.jsx
@@ -12,22 +12,32 @@ const UpdateTask = (props) => {
                         placeholder="Task"
                         name="title"
                         type="text"
-                        required
-                        pattern="\S(.*\S)?"
+                        // required
+                        // pattern="\S(.*\S)?"
                     ></input>
                     <textarea
                         className="placeholder:italic w-100% h-28 focus:outline-none placeholder:text-sm pl-1"
                         placeholder="Description"
                         name="description"
-                        required
-                        pattern="\S(.*\S)?"
+                        // required
+                        // pattern="\S(.*\S)?"
                         type="text"
                     ></textarea>
-                    <input type="date" name="date" required></input>
+                    <input type="date" name="date"></input>
                 </div>
-                <div className="flex gap-2 ml-2 mt-3">
-                    <input type="submit" value="Update"></input>
-                    <button onClick={props.closeUpdate}>Cancel</button>
+                <div className="flex gap-2 ml-2 mt-3 mb-5">
+                    <input
+                        type="submit"
+                        value="Update"
+                        className="btn btn-sm bg-[#DB4C3F] border-0 rounded-lg"
+                    ></input>
+                    <button
+                        type="button"
+                        onClick={props.closeUpdate}
+                        className="bg-[#E5E5E5] rounded-lg text-sm pl-2 pr-2"
+                    >
+                        Cancel
+                    </button>
                 </div>
             </form>
         </div>
