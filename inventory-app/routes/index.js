dc5fecbe01dc2fea634952f77113d462cc42cee9
Daniel Lee <daniel.lee@circleci.com>
2022-07-19 23:05:45 -0700

    where http routes live

--- inventory-app/routes/index.js
+++ inventory-app/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let categoryController = require("../controllers/categoryController");
+let creatorController = require("../controllers/creatorController");
+let itemController = require("../controllers/itemController");
+
+router.get("/", function (req, res) {
+    res.render("./pages/index", { title: " Welcome to Tech Catalog" });
 });
+
+// routes for category
+router.get("/category", categoryController.category_list);
+
+// routes for creator
+router.get("/creator");
+
+// routes for items
+router.get("/items");
 
 module.exports = router;
