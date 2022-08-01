c3f961ec325c70810c01128d8202f61016eb6d6d
Daniel Lee <daniel.lee@circleci.com>
2022-07-31 23:50:21 -0700

    adding create and delete functionalities"

--- inventory-app/controllers/creatorController.js
+++ inventory-app/controllers/creatorController.js
@@ -1,5 +1,6 @@
 let async = require("async");
 const { PrismaClient } = require("@prisma/client");
+const { body, validationResult } = require("express-validator");
 
 const prisma = new PrismaClient();
 
@@ -11,7 +12,7 @@ exports.creator_list = async function (req, res) {
             },
         })
         .then((results) =>
-            results.map((obj) => ({ ...obj, url: "/creator/" + obj.id }))
+            results.map((res) => ({ ...res, url: "/creator/" + res.id }))
         );
 
     res.render("./pages/creator_list", { title: "List of Creators", creators });
@@ -26,3 +27,100 @@ exports.creator_detail = async function (req, res) {
 
     res.render("./pages/creator_detail", { title: creator.name, creator });
 };
+
+exports.create_creator_get = function (req, res) {
+    res.render("./pages/creator_form", { title: "Add Creator" });
+};
+
+exports.create_creator_post = [
+    body("name").trim().isLength({ min: 1 }).escape(),
+    body("description").trim().isLength({ min: 1 }).escape(),
+    body("founded").trim().isLength({ min: 1 }).escape(),
+
+    async function (req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render("./pages/creator_form", {
+                title: "Add Creator",
+                errors: errors.array(),
+            });
+        } else {
+            await prisma.creator.create({
+                data: {
+                    name: req.body.name,
+                    description: req.body.description,
+                    founded: req.body.founded,
+                },
+            });
+
+            res.redirect("/creator");
+        }
+    },
+];
+
+exports.delete_creator_get = function (req, res, next) {
+    async.parallel(
+        {
+            creator: async function (callback) {
+                return await prisma.creator.findUnique({
+                    where: {
+                        id: req.params.id,
+                    },
+                });
+            },
+
+            items: async function (callback) {
+                return await prisma.item.findMany({
+                    where: {
+                        creator_id: req.params.id,
+                    },
+                });
+            },
+        },
+        function (err, results) {
+            if (err) {
+                return next(err);
+            }
+
+            res.render("./pages/creator_delete", {
+                title: "Delete Creator",
+                creator: results.creator,
+                items: results.items,
+            });
+        }
+    );
+};
+
+// exports.delete_creator_post = function (req, res, next) {
+//     async.parallel(
+//         {
+//             creator: async function (callback) {
+//                 await prisma.creator.findUnique({
+//                     where: {
+//                         id: req.params.id,
+//                     },
+//                 });
+//             },
+
+//             items: async function (callback) {
+//                 await prisma.item.findMany({
+//                     where: {
+//                         creator_id: req.params.id,
+//                     },
+//                 });
+//             },
+//         },
+//         function (err, results) {
+//             if (err) {
+//                 return next(err);
+//             }
+
+//             res.render("./pages/creator_delete", {
+//                 title: "Delete Creator",
+//                 creator: results.creator,
+//                 items: results.items,
+//             });
+//         }
+//     );
+// };
