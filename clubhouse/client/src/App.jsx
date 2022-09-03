f0fded11143d67c584c3e7a715e859d3576b3124
Daniel Lee <daniel.lee@circleci.com>
2022-09-02 17:56:42 -0700

    frontend app

--- clubhouse/client/src/App.jsx
+++ clubhouse/client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [backendData, setbackendData] = useState([{}]);
+
+    useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setbackendData(data));
+    }, []);
+
     return (
         <div>
-            <h1 class="text-3xl font-bold underline">Hello World!</h1>
+            <h1 className="text-3xl font-bold underline">Hello World!</h1>
+
+            {typeof backendData.tests === "undefined" ? (
+                <p>Loading...</p>
+            ) : (
+                backendData.tests.map((test, i) => {
+                    return <p key={i}>{test}</p>;
+                })
+            )}
         </div>
     );
 }
