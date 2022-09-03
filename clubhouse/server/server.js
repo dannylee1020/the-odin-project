5746c59991f980d4254196f3b6a8830ee0a6a4c7
Daniel Lee <daniel.lee@circleci.com>
2022-09-02 17:56:21 -0700

    backend server

--- clubhouse/server/server.js
+++ clubhouse/server/server.js
@@ -1,11 +1,15 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/api", (req, res) => {
     res.json({ tests: ["testOne", "testTwo", "testThree"] });
 });
 
-app.listen(5000, () => {
-    console.log("server started on port 5000");
+app.listen(3000, () => {
+    console.log("server started on port 3000");
 });
