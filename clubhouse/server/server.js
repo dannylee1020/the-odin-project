aa95c4c78ff878f47acc2d1195536a84cfb4bc32
Daniel Lee <daniel.lee@circleci.com>
2022-09-05 22:22:17 -0700

    main server app

--- clubhouse/server/server.js
+++ clubhouse/server/server.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+var createError = require("http-errors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api", (req, res) => {
-    res.json({ tests: ["testOne", "testTwo", "testThree"] });
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
 });
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
 });
+
+module.exports = app;
