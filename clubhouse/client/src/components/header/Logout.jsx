293c5117c04ff5f635a261fe7b7d5487da0cf3d2
Daniel Lee <daniel.lee@circleci.com>
2022-09-12 21:51:33 -0700

    add prop and function to sign user out from the session

--- clubhouse/client/src/components/header/Logout.jsx
+++ clubhouse/client/src/components/header/Logout.jsx
@@ -1,7 +1,26 @@
-function Logout() {
+function Logout(props) {
+    const userLogOut = async () => {
+        let url = "http://localhost:3000/logout";
+
+        await fetch(url, {
+            method: "get",
+            credentials: "include",
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    };
+
     return (
         <div>
-            <button className="btn btn-primary font-bold">Log out</button>
+            <button
+                className="btn btn-primary font-bold"
+                onClick={() => {
+                    props.setUserAuth();
+                    userLogOut();
+                }}
+            >
+                Log out
+            </button>
         </div>
     );
 }
