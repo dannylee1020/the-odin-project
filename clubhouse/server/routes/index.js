13931a2d597a64a809d96cc2886e3e14c2252bcb
Daniel Lee <daniel.lee@circleci.com>
2022-09-12 21:05:44 -0700

    fix authentication flow and persist user status

--- clubhouse/server/routes/index.js
+++ clubhouse/server/routes/index.js
@@ -8,7 +8,7 @@ let prisma = new PrismaClient();
 
 let formController = require("../controller/formController");
 
-// authentication
+// define authentication strategy for passport
 passport.use(
     new LocalStrategy(async (username, password, done) => {
         let existUser = await prisma.user.findFirst({
@@ -41,45 +41,52 @@ passport.use(
 );
 
 passport.serializeUser(function (user, done) {
-    console.log(user);
     done(null, user.id);
 });
 
 passport.deserializeUser(async function (id, done) {
-    await prisma.user
-        .findFirst({
-            where: {
-                id: id,
-            },
-        })
-        .then((err, user) => {
-            done(err, user);
-        });
+    const user = await prisma.user.findFirst({
+        where: {
+            id: id,
+        },
+    });
+
+    done(null, user);
 });
 
 function authenticateLocal(req, res, next) {
     // pass callback function to handle error and get errors configured in LocalStrategy
-    passport.authenticate("local", function (err, user, info) {
-        if (err) return next(err);
-        if (!user) {
-            return res.status(401).send({
-                error: "Invalid Authorization Request",
-                message: info,
+    passport.authenticate(
+        "local",
+        { successRedirect: "/" },
+        function (err, user, info) {
+            if (err) return next(err);
+            if (!user) {
+                return res.status(401).send({
+                    error: "Invalid Authorization Request",
+                    message: info,
+                });
+            }
+            //manually estabilishes a session for custom callback
+            req.login(user, (err) => {
+                if (err) return next(err);
+                res.send({ message: "authentication successful" });
             });
         }
-        req.user = user;
-        next();
-    })(req, res, next);
+    )(req, res, next);
 }
 
 // authentication route
 router.get("/", (req, res) => {
-    res.send({ user: req.user });
+    // res.send(req.user);
+    if (req.user) {
+        res.status(200).send(req.user);
+    } else {
+        res.status(401).send({ message: "user not authenticated" });
+    }
 });
 
-router.post("/login", authenticateLocal, (req, res) => {
-    res.send(req.user);
-});
+router.post("/login", authenticateLocal);
 
 //sign up route
 router.post("/signup", formController.create_user_post);
