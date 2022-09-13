0aa4d3488b08dde6c289a7381c67c96fc2633527
Daniel Lee <daniel.lee@circleci.com>
2022-09-13 14:41:27 -0700

    hash password at sign up

--- clubhouse/server/controller/formController.js
+++ clubhouse/server/controller/formController.js
@@ -1,6 +1,7 @@
 let async = require("async");
 const { PrismaClient } = require("@prisma/client");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
@@ -12,25 +13,30 @@ exports.create_user_post = [
 
     async function (req, res) {
         const errors = validationResult(req);
+        bcrypt.hash(req.body.password, 10, async (err, pw) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (!errors.isEmpty()) {
+                    return res.status(422).json({ errors: errors.array() });
+                } else {
+                    await prisma.user.create({
+                        data: {
+                            first_name: req.body.first_name,
+                            last_name: req.body.last_name,
+                            username: req.body.username,
+                            auth: {
+                                create: { password: pw },
+                            },
+                        },
+                        include: {
+                            auth: true,
+                        },
+                    });
 
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        } else {
-            await prisma.user.create({
-                data: {
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    username: req.body.username,
-                    auth: {
-                        create: { password: req.body.password },
-                    },
-                },
-                include: {
-                    auth: true,
-                },
-            });
-
-            res.send(res.status(200).json({ message: "request successful" }));
-        }
+                    res.status(200).json({ message: "request successful" });
+                }
+            }
+        });
     },
 ];
