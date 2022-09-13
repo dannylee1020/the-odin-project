dad3faba6a110945cca3997dd52c8fb50b62b97b
Daniel Lee <daniel.lee@circleci.com>
2022-09-12 21:50:25 -0700

    add function for checking user auth status and push down as a prop

--- clubhouse/client/src/components/header/Header.jsx
+++ clubhouse/client/src/components/header/Header.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Login from "./Login";
+import Logout from "./Logout";
 import Signup from "./Signup";
 
 const Header = () => {
+    const [userStatus, setUserStatus] = useState(false);
+
+    const setUserAuth = () => {
+        userStatus === false ? setUserStatus(true) : setUserStatus(false);
+    };
+
     return (
         <div className="flex justify-between">
             <div className="text-primary font-bold text-3xl">
                 Club<span className="text-black">House</span>
             </div>
             <div></div>
-            <div className="flex gap-3">
-                <Login></Login>
-                <Signup></Signup>
-            </div>
+            {userStatus ? (
+                <div className="flex">
+                    <Logout setUserAuth={setUserAuth}></Logout>
+                </div>
+            ) : (
+                <div className="flex gap-3">
+                    <Login setUserAuth={setUserAuth}></Login>
+                    <Signup></Signup>
+                </div>
+            )}
         </div>
     );
 };
