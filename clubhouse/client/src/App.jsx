4e10253861149be95fe0aba2f16fb49856f61eec
Daniel Lee <daniel.lee@circleci.com>
2022-09-13 22:58:31 -0700

    route components to different path

--- clubhouse/client/src/App.jsx
+++ clubhouse/client/src/App.jsx
@@ -1,10 +1,20 @@
 import Header from "./components/header/Header";
+import PrivatePage from "./components/private/PrivatePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div className="mx-5 mt-5">
-            <Header></Header>
-        </div>
+        <BrowserRouter>
+            <div className="mx-5 mt-5">
+                <Routes>
+                    <Route path="/" element={<Header></Header>}></Route>
+                    <Route
+                        path="/private"
+                        element={<PrivatePage></PrivatePage>}
+                    ></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
