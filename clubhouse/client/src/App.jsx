eda631a29c393f6fbc5f1a197bb36d7bace91351
Daniel Lee <daniel.lee@circleci.com>
2022-09-15 23:05:24 -0700

    add component routing and passdown some props

--- clubhouse/client/src/App.jsx
+++ clubhouse/client/src/App.jsx
@@ -1,16 +1,32 @@
 import Header from "./components/header/Header";
 import PrivatePage from "./components/private/PrivatePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+    const setUserAuth = () => {
+        const userStatus = localStorage.getItem("status");
+
+        userStatus !== true
+            ? localStorage.setItem("status", true)
+            : localStorage.setItem("status", false);
+    };
+
     return (
         <BrowserRouter>
             <div className="mx-5 mt-5">
                 <Routes>
-                    <Route path="/" element={<Header></Header>}></Route>
+                    <Route
+                        path="/"
+                        element={<Header setUserAuth={setUserAuth}></Header>}
+                    ></Route>
                     <Route
                         path="/private"
-                        element={<PrivatePage></PrivatePage>}
+                        element={
+                            <PrivatePage
+                                setUserAuth={setUserAuth}
+                            ></PrivatePage>
+                        }
                     ></Route>
                 </Routes>
             </div>
