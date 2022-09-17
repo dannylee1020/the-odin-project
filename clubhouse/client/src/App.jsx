750ba173be9ae637b332f29ca5468d9d40f50d90
Daniel Lee <daniel.lee@circleci.com>
2022-09-16 18:05:27 -0700

    remove setting user status no longer needed here

--- clubhouse/client/src/App.jsx
+++ clubhouse/client/src/App.jsx
@@ -1,32 +1,17 @@
 import Header from "./components/header/Header";
 import PrivatePage from "./components/private/PrivatePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const setUserAuth = () => {
-        const userStatus = localStorage.getItem("status");
-
-        userStatus !== true
-            ? localStorage.setItem("status", true)
-            : localStorage.setItem("status", false);
-    };
-
     return (
         <BrowserRouter>
             <div className="mx-5 mt-5">
                 <Routes>
-                    <Route
-                        path="/"
-                        element={<Header setUserAuth={setUserAuth}></Header>}
-                    ></Route>
+                    <Route path="/" element={<Header></Header>}></Route>
                     <Route
                         path="/private"
-                        element={
-                            <PrivatePage
-                                setUserAuth={setUserAuth}
-                            ></PrivatePage>
-                        }
+                        element={<PrivatePage></PrivatePage>}
                     ></Route>
                 </Routes>
             </div>
