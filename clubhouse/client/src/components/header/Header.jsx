d6516e051702fabe94c7a8899fc2ea56622b7767
Daniel Lee <daniel.lee@circleci.com>
2022-09-19 15:48:04 -0700

    remove function and receive props from parent instead

--- clubhouse/client/src/components/header/Header.jsx
+++ clubhouse/client/src/components/header/Header.jsx
@@ -1,29 +1,17 @@
-import { useState, useEffect } from "react";
 import Login from "./Login";
 import Logout from "./Logout";
 import Signup from "./Signup";
 
 const Header = (props) => {
-    const [userStatus, setUserStatus] = useState(false);
-
-    useEffect(() => {
-        fetch("http://localhost:3000/", {
-            method: "get",
-            credentials: "include",
-        }).then((res) =>
-            res.status !== 401 ? setUserStatus(true) : setUserStatus(false)
-        );
-    }, []);
-
     return (
         <div className="flex justify-between">
             <div className="text-primary font-bold text-3xl">
                 Club<span className="text-black">House</span>
             </div>
             <div></div>
-            {userStatus ? (
+            {props.userStatus ? (
                 <div className="flex">
-                    <Logout onClick={() => setUserStatus(false)}></Logout>
+                    <Logout onClick={props.onClick}></Logout>
                 </div>
             ) : (
                 <div className="flex gap-3">
