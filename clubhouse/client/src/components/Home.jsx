fc0d2a4ed198e6ba53c0b1ba7745db3c676eeec4
Daniel Lee <daniel.lee@circleci.com>
2022-09-19 15:46:18 -0700

    create a function to check user status and pass down as props

--- clubhouse/client/src/components/Home.jsx
+++ clubhouse/client/src/components/Home.jsx
@@ -1,16 +1,29 @@
 import Header from "./header/Header";
 import Message from "./messages/Message";
 import MessageForm from "./messages/MessageForm";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
+    const [userStatus, setUserStatus] = useState(false);
+
     useEffect(() => {
-        getMessages();
+        getUserStatus();
     }, []);
 
+    // check if user is logged in
+    async function getUserStatus() {
+        await fetch("http://localhost:3000/", {
+            method: "GET",
+            credentials: "include",
+        }).then((res) =>
+            res.status !== 401 ? setUserStatus(true) : setUserStatus(false)
+        );
+    }
+
+    // get all the messages to display in home page
     async function getMessages() {
         let messages = await fetch("http://localhost:3000/messages", {
-            method: "get",
+            method: "GET",
             credentials: "include",
         });
 
@@ -20,13 +33,19 @@ function Home() {
     return (
         <div className="flex flex-col justify-between h-5/6">
             <div>
-                <Header />
+                <Header
+                    getUserStatus={getUserStatus}
+                    onClick={() => setUserStatus(false)}
+                    userStatus={userStatus}
+                />
                 <Message getMessages={getMessages} />
             </div>
             <div></div>
-            <div>
-                <MessageForm getMessages={getMessages} />
-            </div>
+            {userStatus ? (
+                <div>
+                    <MessageForm getMessages={getMessages} />
+                </div>
+            ) : null}
         </div>
     );
 }
