ec37fbaddfb31a05d160a51596248654eef2a520
Daniel Lee <daniel.lee@circleci.com>
2022-09-20 20:42:07 -0700

    map all messages from db to display on home page

--- clubhouse/client/src/components/messages/Message.jsx
+++ clubhouse/client/src/components/messages/Message.jsx
@@ -1,17 +1,27 @@
 function Message(props) {
-    let messages = props.getMessages();
+    function generateKey(pre) {
+        return `${pre}_${new Date().getTime()}`;
+    }
 
     return (
-        <div></div>
-        // <div className="flex flex-col items-center">
-        //     {messages.map((message) => {
-        //         return (
-        //             <div className="bg-slate-200 rounded-lg w-1/3 h-10">
-        //                 <span>{message}</span>
-        //             </div>
-        //         );
-        //     })}
-        // </div>
+        <div>
+            <div className="flex flex-col items-center gap-5 mt-10">
+                {props.messages === undefined ? (
+                    <div>Loading...</div>
+                ) : (
+                    props.messages.map((m) => {
+                        return (
+                            <div
+                                className="bg-slate-200 rounded-lg w-1/3 h-10"
+                                key={generateKey(m.id)}
+                            >
+                                <span>{m.message}</span>
+                            </div>
+                        );
+                    })
+                )}
+            </div>
+        </div>
     );
 }
 
