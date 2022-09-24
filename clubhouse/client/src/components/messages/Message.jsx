37a0a578f0a189da9feccb941afaf518594f5722
Daniel Lee <daniel.lee@circleci.com>
2022-09-23 23:57:38 -0700

    fix message container styling

--- clubhouse/client/src/components/messages/Message.jsx
+++ clubhouse/client/src/components/messages/Message.jsx
@@ -5,17 +5,17 @@ function Message(props) {
 
     return (
         <div>
-            <div className="flex flex-col items-center gap-5 mt-10">
+            <div className="flex flex-col items-center gap-5 mt-10 mb-10">
                 {props.messages === undefined ? (
                     <div>Loading...</div>
                 ) : (
                     props.messages.map((m) => {
                         return (
                             <div
-                                className="bg-slate-200 rounded-lg w-1/3 h-10"
+                                className="bg-slate-200 rounded-lg w-1/3 whitespace-normal break-normal"
                                 key={generateKey(m.id)}
                             >
-                                <span>{m.message}</span>
+                                <div className="p-8">{m.message}</div>
                             </div>
                         );
                     })
