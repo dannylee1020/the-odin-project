70c949d751b9aa09158193de8a95046fee3f9c66
Daniel Lee <daniel.lee@circleci.com>
2022-09-27 21:40:09 -0700

    add hint as a modal

--- clubhouse/client/src/components/private/PrivatePage.jsx
+++ clubhouse/client/src/components/private/PrivatePage.jsx
@@ -41,13 +41,46 @@ function PrivatePage(props) {
                 </h1>
                 <form onSubmit={handleSubmit}>
                     <div className="flex flex-col items-center">
-                        <input
-                            type="text"
-                            name="passcode"
-                            className="w-full h-8 border-2 focus:border-primary focus:outline-none mb-7"
-                            onChange={handleChange}
-                            required
-                        ></input>
+                        <div className="w-full flex gap-5 justify-center">
+                            <input
+                                type="text"
+                                name="passcode"
+                                className="w-full h-8 border-2 focus:border-primary focus:outline-none mb-7"
+                                onChange={handleChange}
+                                required
+                            ></input>
+                            <div>
+                                <label
+                                    htmlFor="hint"
+                                    className="btn modal-button btn-sm bg-[#FAF7F5] border-0 text-[#40ABB0] hover:bg-[#FAF7F5]"
+                                >
+                                    ?!
+                                </label>
+                                <input
+                                    type="checkbox"
+                                    id="hint"
+                                    className="modal-toggle"
+                                ></input>
+                                <div className="modal">
+                                    <div className="modal-box relative">
+                                        <label
+                                            htmlFor="hint"
+                                            className="btn btn-sm btn-circle right-2 top-2 modal-button absolute bg-[#FAF7F5] border-0 text-black hover:bg-[#FAF7F5] text-slate-600"
+                                        >
+                                            x
+                                        </label>
+                                        <h3 className="text-lg font-bold">
+                                            Hint
+                                        </h3>
+                                        <p className="py-4 font-serif">
+                                            What is a magical phrase from the
+                                            story of "Ali Baba and the Forty
+                                            Thieves" ?
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <button className="btn btn-primary w-full text-white font-bold">
                             Submit
                         </button>
