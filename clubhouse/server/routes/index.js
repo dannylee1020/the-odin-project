a86c25e93999dda1cda448ff8dd8447ebd388255
Daniel Lee <daniel.lee@circleci.com>
2022-09-28 21:28:13 -0700

    change endpoint path

--- clubhouse/server/routes/index.js
+++ clubhouse/server/routes/index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/user", (req, res) => {
     if (req.user) {
         res.status(200).send(req.user);
     } else {
